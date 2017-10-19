36d553103aec31e381a517168e65ae277d0118eb
mixelpixel <mixelpix@mac.com>
2017-10-19 00:43:27 -0400

    idem

--- patrick/BananaTranspiler.js
+++ patrick/BananaTranspiler.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 const os = require('os');
 
 /* 1. Read "BananaScript.bnf" file from an argument to a node program BananaTranspiler.js
- * script name to be read in from node argument value
+ * script name to be read in from node 3rd argument value
  * e.g. node BananaTranspiler.js BananaScript.bnf */
 const bnfScriptName = process.argv[2];
 console.log(`1. File to be ingested and parsed: "${bnfScriptName}"`);
 
-/* 2. Read each line of file, separating by tokens, that is: stuff in <>, :=, |
+/* 2. Read each line of file...
  * read in file contents, separating each line into an array of strings */
 const bnfScript = fs.readFileSync(bnfScriptName, 'utf8').split(os.EOL);
 console.log('2. Array containing each line from text file:\n', bnfScript);
@@ -18,6 +18,7 @@ console.log('2. Array containing each line from text file:\n', bnfScript);
 
 /* 3. Put the tokens from the read into a hash table "BNF_table" by the left side of the line delimited by := */
 console.log('3. TO BE DONE');
+
 /* 4. Read "BananaScript.bs" file containing custom bananascript programming language */
 const bananaScript = fs.readFileSync('./BananaScript.bs', 'utf8').split(os.EOL);
 console.log('4. Array containing each line from text file:\n', bananaScript);
