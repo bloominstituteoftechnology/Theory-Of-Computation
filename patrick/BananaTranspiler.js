3d0c736b38324f473eab2740a55dbc6d7b6a316a
mixelpixel <mixelpix@mac.com>
2017-10-18 21:15:34 -0400

    require process statement to make linter happy

--- patrick/BananaTranspiler.js
+++ patrick/BananaTranspiler.js
@@ -1,1 +1,4 @@
-// https://www.lucidchart.com/documents/edit/c7e701b5-f2f4-4c20-ab8b-4fea175c5cf5/0?shared=true&
+'use-strict';
+/* eslint no-console: 0 */
+
+const process = require('process'); // <~~~ Not needed, just making linter happy
