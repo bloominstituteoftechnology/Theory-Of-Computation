5a7dca59d2ed92a8b2b1b53fe0a47d50de434582
mixelpixel <mixelpix@mac.com>
2017-10-18 21:19:23 -0400

    ingest script name from node process argv

--- patrick/BananaTranspiler.js
+++ patrick/BananaTranspiler.js
@@ -2,3 +2,8 @@
 /* eslint no-console: 0 */
 
 const process = require('process'); // <~~~ Not needed, just making linter happy
+
+/* 1. script name to be read in from node argument value
+ * e.g. node BananaTranspiler.js BananaScript.bnf */
+const bnfScriptName = process.argv[2];
+console.log(bnfScriptName);
