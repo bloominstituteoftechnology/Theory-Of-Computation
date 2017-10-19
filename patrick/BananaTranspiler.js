d8839d7ec29513fda00141d5f4e3f30ce535fde4
mixelpixel <mixelpix@mac.com>
2017-10-18 21:31:49 -0400

    improve console.log statements

--- patrick/BananaTranspiler.js
+++ patrick/BananaTranspiler.js
@@ -4,12 +4,11 @@ const process = require('process'); // <~~~ Not needed, just making linter happy
 const fs = require('fs');
 const os = require('os');
 
-
 /* 1. script name to be read in from node argument value
  * e.g. node BananaTranspiler.js BananaScript.bnf */
 const bnfScriptName = process.argv[2];
-console.log(bnfScriptName);
+console.log(`1. File to be ingested and parsed: "${bnfScriptName}"`);
 
 /* 2. read in file contents, separating each line into an array of strings */
 const bnfScript = fs.readFileSync(bnfScriptName, 'utf8').split(os.EOL);
-console.log(bnfScript);
+console.log('2. Array containing each line from text file:\n', bnfScript);
