63f90109dc8e4d19eda28747dcb9a5422209cc94
mixelpixel <mixelpix@mac.com>
2017-10-19 18:24:23 -0400

    for lecture

--- patrick/BananaTranspiler.js
+++ patrick/BananaTranspiler.js
@@ -4,36 +4,36 @@ const process = require('process'); // <~~~ Not needed, just making linter happy
 const fs = require('fs');
 const os = require('os');
 
-// /******************************************************************************
-//  * 1. Read "BananaScript.bnf" file from an argument to a node program
-//  * "BananaTranspiler.js"
-//  * Script name to be read in from node 3rd argument value
-//  * e.g. `$ node BananaTranspiler.js BananaScript.bnf`
-//  ******************************************************************************/
-// const bnfScriptFile = process.argv[2];
-// console.log(`1. File to be ingested and parsed: "${bnfScriptFile}"`);
-//
-//
-// /******************************************************************************
-//  * 2. Read each line of file...
-//  * read in file contents, separating each line into an array of strings
-//  * NOTE: should this file be entered as a fourth parameter?
-//  ******************************************************************************/
-// const bnfScript = fs.readFileSync(bnfScriptFile, 'utf8').split(os.EOL);
-// console.log('2. Array containing each line from text file:\n', bnfScript);
-// /* ...separating by tokens, that is: stuff in <>, :=, | */
-// for (let i = 0; i < bnfScript.length; i++) {
-//   // remove white spaces and empty strings?
-//   // parse each string into components
-//
-// }
-//
-//
-// /******************************************************************************
-//  * 3. Put the tokens from the read into a hash table
-//  * "BNF_table" by the left side of the line delimited by :=
-//  ******************************************************************************/
-// console.log('3. TO BE DONE');
+/******************************************************************************
+ * 1. Read "BananaScript.bnf" file from an argument to a node program
+ * "BananaTranspiler.js"
+ * Script name to be read in from node 3rd argument value
+ * e.g. `$ node BananaTranspiler.js BananaScript.bnf`
+ ******************************************************************************/
+const bnfScriptFile = process.argv[2];
+console.log(`1. File to be ingested and parsed: "${bnfScriptFile}"`);
+
+
+/******************************************************************************
+ * 2. Read each line of file...
+ * read in file contents, separating each line into an array of strings
+ * NOTE: should this file be entered as a fourth parameter?
+ ******************************************************************************/
+const bnfScript = fs.readFileSync(bnfScriptFile, 'utf8').split(os.EOL);
+console.log('2. Array containing each line from text file:\n', bnfScript);
+/* ...separating by tokens, that is: stuff in <>, :=, | */
+for (let i = 0; i < bnfScript.length; i++) {
+  // remove white spaces and empty strings?
+  // parse each string into components
+
+}
+
+
+/******************************************************************************
+ * 3. Put the tokens from the read into a hash table
+ * "BNF_table" by the left side of the line delimited by :=
+ ******************************************************************************/
+console.log('3. TO BE DONE');
 
 
 /******************************************************************************
@@ -65,20 +65,20 @@ if (bananaSplit[0][0] === stringObj[1]) {
 }
 
 
-// /******************************************************************************
-//  * 5. Parse it using a pushdown automata and accumulate
-//  * the results into a string variable program
-//  ******************************************************************************/
-// console.log('5. TO BE DONE');
-//
-//
-// /******************************************************************************
-//  * 6. let bananascript_executable = eval(program);
-//  ******************************************************************************/
-// console.log('6. TO BE DONE');
-//
-//
-// /******************************************************************************
-//  * 7. program();
-//  ******************************************************************************/
-// console.log('7. TO BE DONE');
+/******************************************************************************
+ * 5. Parse it using a pushdown automata and accumulate
+ * the results into a string variable program
+ ******************************************************************************/
+console.log('5. TO BE DONE');
+
+
+/******************************************************************************
+ * 6. let bananascript_executable = eval(program);
+ ******************************************************************************/
+console.log('6. TO BE DONE');
+
+
+/******************************************************************************
+ * 7. program();
+ ******************************************************************************/
+console.log('7. TO BE DONE');
