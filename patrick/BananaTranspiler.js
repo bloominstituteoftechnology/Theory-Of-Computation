ce8c7abbff32c6ad6255f45e5b40ffe7f056b9f7
mixelpixel <mixelpix@mac.com>
2017-10-19 21:55:17 -0400

    organizizing

--- patrick/BananaTranspiler.js
+++ patrick/BananaTranspiler.js
@@ -17,34 +17,28 @@ console.log(`1. File to be ingested and parsed: "${bnfScriptFile}"`);
 /******************************************************************************
  * 2. Read each line of file...
  * read in file contents, separating each line into an array of strings
- * NOTE: should this file be entered as a fourth parameter?
  ******************************************************************************/
 const bnfScriptOriginal = fs.readFileSync(bnfScriptFile, 'utf8').split(os.EOL);
-console.log('2. Array containing each line from text file:\n', bnfScriptOriginal);
-/* ...separating by tokens, that is: stuff in <>, :=, | */
-for (let i = 0; i < bnfScriptOriginal.length; i++) {
-  // remove white spaces and empty strings?
-  // parse each string into components
+console.log('2. Array containing each line from BNF text file:\n', bnfScriptOriginal);
 
-}
 
 /******************************************************************************
+ * 2. ...separating by tokens, that is: stuff in <>, :=, |
  * 3. Put the tokens from the read into a hash table
- * "BNF_table" by the left side of the line delimited by :=
+ *    "BNF_table" by the left side of the line delimited by :=
  ******************************************************************************/
-console.log('3. Put the tokens from the read into a hash table');
-/* ...separating by tokens, that is: stuff in <>, :=, | */
+console.log('3. Put the tokens from the BNF file into a hash table');
 let map = {};
 let previousToken = undefined;
 for (let i = 0; i < bnfScriptOriginal.length; i++) {
-  // remove white spaces and empty strings?
+  // remove white spaces and empty strings
   // parse each string into components
-  const newLine = bnfScriptOriginal[i].split(":=");
+  const newLine = bnfScriptOriginal[i].split(':=');
   if(newLine[1] != undefined) { // there is a LHS and a RHS of the BNF rule
     let token = newLine[0].trim();
     previousToken = token;
     let newExpansions = newLine[1].split('|');
-    newExpansions = newExpansions.map((exp) => {return exp.trim()});
+    newExpansions = newExpansions.map((exp) => {return exp.trim();});
     let existingExpansion = map[token];
     if(existingExpansion === undefined) {
       map[token] = newExpansions;
@@ -57,16 +51,19 @@ for (let i = 0; i < bnfScriptOriginal.length; i++) {
     map[previousToken].push(newLine[0].trim());
   }
 }
-console.log('This is the table of tokens: ');
+// console.log('This is the table of tokens: ');
 console.log(map);
 
 
 /******************************************************************************
  * 4. Read "BananaScript.bs" file containing
  * custom bananascript programming language
+ * NOTE: should this file be entered as a fourth parameter?
+ * e.g. `$ node BananaTranspiler.js BananaScript.bnf BananaScript.bs`
  ******************************************************************************/
 const bananaScript = fs.readFileSync('./BananaScript.bs', 'utf8').split(os.EOL);
-// console.log('4. Array containing each line from text file:\n', bananaScript);
+console.log('4. Array containing each line from text file:\n', bananaScript);
+
 let bananaSplit = [];
 let carrotStrip;
 for (let i = 0; i < bananaScript.length; i++){
@@ -75,20 +72,19 @@ for (let i = 0; i < bananaScript.length; i++){
 }
 console.log(bananaSplit);
 
-const stringObj = {
-  1: 'd_string',
-  2: 'let',
-};
-
-let jsString = '';
-
-if (bananaSplit[0][0] === stringObj[1]) {
-  console.log('Jesh doesnt share his cheap candy');
-  // let newJS = jsString.concat(stringObj[2], );
-  bananaSplit[0][0] = stringObj[2];
-  console.log(bananaSplit);
-}
-
+// const stringObj = {
+//   1: 'd_string',
+//   2: 'let',
+// };
+//
+// // let jsString = '';
+//
+// if (bananaSplit[0][0] === stringObj[1]) {
+//   console.log('Jesh doesnt share his cheap candy');
+//   // let newJS = jsString.concat(stringObj[2], );
+//   bananaSplit[0][0] = stringObj[2];
+//   console.log(bananaSplit);
+// }
 
 /******************************************************************************
  * 5. Parse it using a pushdown automata and accumulate
