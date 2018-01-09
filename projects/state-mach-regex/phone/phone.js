ccf32bd3c5dd42a1422e640221a0588fc4e62b57
ceylonsullen <hussainaljahmi@gmail.com>
2018-01-09 08:45:15 -0500

    got phone, trouble with links

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,15 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+    const reg = /(\d{3}).*(\d{3}).*(\d{4})/;
     // Find matches
-
+      if (reg.test(line)) {
+        const match = line.match(reg);
+        console.log(match[1] + match[2] + match[3]);
+      } else {
+        console.log('number not found!');
+      }
     // If match found, print number with no spaces, parens, or dashes
-
+      
     // Else print that no number was found
 });
