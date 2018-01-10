8f6a8ac958762a3679f20ee1ee27b79db9320c5a
Frank Faustino <nawzyah@gmail.com>
2018-01-09 23:53:44 -0800

    extractlinks.js, phone.js, turn_signal_state_machine.png completed

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,20 +1,13 @@
 const fs = require('fs');
 
-const args = process.argv.slice(2);
+const file = process.argv[2];
 
-if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
-}
-
-const filename = args[0];
-
-// !!!! IMPLEMENT ME
-
-// Read file
-
-// Set up regex
-
-// Find matches
-
-// Print all matches
+file
+  ? fs.readFile(file, 'utf8', (err, data) => {
+      if (err) console.error(`error: no such file '${file}'`);
+      else
+        data
+          .match(/https?:\/\/[^'"]+\.[^'"]+/g)
+          .forEach(url => console.log(url));
+    })
+  : console.error('usage: extractlinks inputfile');
