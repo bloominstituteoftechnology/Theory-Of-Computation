dca1db3b75a734075f156cddfff8208d3b48f7af
invegat <mark.c.oliver@gmail.com>
2018-02-12 19:15:43 -0500

    done

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,3 +1,4 @@
+const debugging = false;
 const fs = require('fs');
 
 const args = process.argv.slice(2);
@@ -8,6 +9,25 @@ if (args.length != 1) {
 }
 
 const filename = args[0];
+const stream = debugging ? fs.createWriteStream('./urls.txt', 'utf8') : null;
+
+fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+
+        console.log(`error: ${err}`);
+        exit(1);
+    }
+    // const urlReg =  RegExp('(?:href|content)\s*\=\s*\"(https?\:\/\/[^\"]+)\"','g');
+    const urlReg = RegExp('(https?\:\/\/.+?)[\,\)\'\"]', 'g');
+    let match;
+    while ((match = urlReg.exec(data)) !== null) {  // data.match(/(?:href|source)\s*\=\s*\"(https?\:\/\/[^\"]+)\"/g);
+        console.log(match[1]);
+        if (debugging)
+            stream.write(match[1] + '\n');
+    }
+    if (debugging)
+        stream.close();
+});
 
 // !!!! IMPLEMENT ME
 
--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -10,6 +10,12 @@ var rl = readline.createInterface({
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
+  let matches = line.match(/^\s*(?:\+(\d+)\s+)?\(?(\d{3})\:?\)?\-?\s*(\d{3})\-?(\d{4})/);
+  if (matches && (matches.length == 4 || matches.length == 5))  {
+    console.log(`${matches[1] != undefined ? matches[1] : ""}${matches[2]}${matches[3]}${matches[4]}`);
+  }
+  else 
+    console.log("no number was found")
 
     // !!!! IMPLEMENT ME
 
