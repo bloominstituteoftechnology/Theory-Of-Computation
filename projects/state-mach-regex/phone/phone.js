27300afcedc1261ba1a5bac02a7187a4125cdd97
Jason Nuhn <jason.a.nuhn@gmail.com>
2018-02-13 18:55:16 -0500

    work completed

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -7,17 +7,23 @@ var rl = readline.createInterface({
 });
 
 
-// This code reads a line at a time from stdin
+// // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
 
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+    const regex = /([0-9])\w+/g;
     // Find matches
-
+    var found = line.match(regex);
     // If match found, print number with no spaces, parens, or dashes
-
+    if (found !== null) {
+      console.log(`The phone number is ${line}`);
+      rl.close();
+    } else {
+      // Else print that no number was found
+      console.log("No Number was found");
+    }
     // Else print that no number was found
 });
