e1e1bb1671d2b7f4fa03e9d949b896c5b8960dbe
Teresa Strout <qapla47@gmail.com>
2018-02-14 00:07:24 -0500

    all working

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -9,12 +9,37 @@ if (args.length != 1) {
 
 const filename = args[0];
 
-// !!!! IMPLEMENT ME
+// start with https and pull all characters until you see a quotation, unless it's whitespace
+const regEx = /(https:\/\/\S+(?=\"))/gi
 
-// Read file
+fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) console.log(err);
+        // exit(1);
 
-// Set up regex
+    let found; 
+    while ((found = data.match(regEx)) !== null) {
+        console.log(found);
+    }
+});
 
-// Find matches
 
-// Print all matches
+// this works in repl.it, passing the entire contents of the file in as a string literal
+// const regEx = /(https:\/\/\S+(?=\"))/gi
+// function parseMe(data) {
+//         let found = data.match(regEx);
+//     console.log(found);
+// }
+// parseMe(
+//   `<!DOCTYPE html>
+// <html>
+
+//     <head>
+
+//         <title>Stack Overflow - Where Developers Learn, Share, &amp; Build Careers</title>
+//         <link rel="shortcut icon" href="https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico?v=4f32ecc8f43d">
+//         <link rel="apple-touch-icon image_src" href="https://cdn.sstatic.net/Sites/stackoverflow/img/apple-touch-icon.png?v=c78bd457575a">
+//         <link rel="search" type="application/opensearchdescription+xml" title="Stack Overflow" href="/opensearch.xml">
+//         <meta name="description" content="Stack Overflow is the largest, most trusted online community for developers to learn, share​ ​their programming ​knowledge, and build their careers."/>
+
+
+//         <meta property="og:type" content="website"/>...`);
--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,16 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+  
     // Find matches
+  if (line.replace(/[^\d]/g, '').length <= 10) {
+    return line.replace(/[^\d]/g, '');
+  } return ('no number found')
 
+  // line.replace(/[^\d]/g, '').length <= 10 ? line.replace(/[^\d]/g, '') : 'no number found';
     // If match found, print number with no spaces, parens, or dashes
 
     // Else print that no number was found
 });
+
+// returns only phone numbers with 10 characters or less, with only digits present
