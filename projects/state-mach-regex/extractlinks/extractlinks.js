db9481a7a10d4107ac9027881db3d5beea7cc53f
Jesse Hood <jessenoob@gmail.com>
2018-03-19 16:07:02 -0400

    completed extractlinks

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,20 +1,30 @@
-const fs = require('fs');
-
+const fs = require("fs");
+const { promisify } = require("util");
 const args = process.argv.slice(2);
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+  console.error("usage: extractlinks inputfile");
+  process.exit(1);
 }
 
 const filename = args[0];
 
 // !!!! IMPLEMENT ME
 
-// Read file
+try {
+  // Read file
+  const fileString = fs.readFileSync(filename, { encoding: 'utf8' });
+  
+  // Set up regex
+  const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
-// Set up regex
+  // Find matches
+  const urls = fileString.match(urlRegex);
+  if (!urls.length) throw 'No urls found.';
 
-// Find matches
+  // Print all matches
+  urls.forEach(url => console.log(url));
 
-// Print all matches
+} catch(err) {
+  console.log(err.message);
+}
