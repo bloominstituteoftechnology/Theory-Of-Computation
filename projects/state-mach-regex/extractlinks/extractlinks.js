6fa99d80f4266eab61244445289b287635ac0518
JASPINDER SING <jaspindersingh83@gmail.com>
2018-03-19 15:45:51 -0600

    Regex Complete

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -8,13 +8,28 @@ if (args.length != 1) {
 }
 
 const filename = args[0];
-
 // !!!! IMPLEMENT ME
 
 // Read file
+let content;
+content = fs.readFileSync('./stackoverflow.html','utf-8');
+//Async version
+// fs.readFile('./stackoverflow.html', (err,data) => {
+//     if(err) throw err;
+//     content = data;
+//     processFile();
+// })
+
+// function processFile(){
+    
+// }
 
 // Set up regex
+const regex = /(https?):\/\/[0-9?a-z./=-]+/gi
 
 // Find matches
-
+let matchesArr = content.match(regex);
 // Print all matches
+for(let i = 0; i < matchesArr.length; i++){
+    console.log(matchesArr[i])
+}
