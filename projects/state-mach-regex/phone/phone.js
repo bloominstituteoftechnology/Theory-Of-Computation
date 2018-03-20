0ecc7d21690b5ea6fcf894a9a852e80ee4dba81c
Moises Dobarganes <designerexpert@gmail.com>
2018-03-20 19:02:45 -0400

    Phone Numbers RegEx Finished for USA Style Phone Numbers

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -12,17 +12,31 @@ var rl = readline.createInterface({
 rl.on('line', function (line) {
 
   // !!!! IMPLEMENT ME
-  let testArr = [
-    '555-123-4567',
-    '5551234567',
-    '(555) 123 - 4567',
-    '555 123 4567'];
+
 
   // Come up with the phone regex
-  let reg = /[^1(0]\d{3}[^-\s)][^-\s)]?\d{3}[^-\s)][^-\s)]?\d{4}/;
-  // Find matches
-
-  // If match found, print number with no spaces, parens, or dashes
+  let reg = /[\W1]?(\d{3})[\W]?[\W]?(\d{3})[\W]?[\W]?[\W]?(\d+)/;
+  // Find matches  
+  /* For Debugging Purposes and Building the Script
+    let testArr = [
+      '555-123-4567',
+      '5551234567',
+      '(555) 123 - 4567',
+      '555 123 4567'];
+    If match found, print number with no spaces, parens, or dashes
+    let returnArray = []
+    testArr.forEach(item => {
+      let extracted = item.match(reg)
+      returnArray.push(extracted[1] + extracted[2] + extracted[3]);
+    })
+    console.log(returnArray);
+  //End of Debugging Section*/
+  let extracted = line.match(reg);
+  if (extracted !== null) {
+    console.log(extracted[1] + extracted[2] + extracted[3]);
+  } else {
+    console.log("No USA Format Phone Number Found");
+  }
 
   // Else print that no number was found
 });
