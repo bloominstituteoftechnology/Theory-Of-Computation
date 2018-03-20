5821bf21c016919b3e5ea763792f8644883a4054
Xiaoping Li <narci2008@gmail.com>
2018-03-20 16:02:47 -0700

    finished assignment

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,20 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const re = /\(?\d+\)?[-.\s]?\d+[-.\s]?\d+/g;
 
     // Find matches
+    const matchArr = line.match(re);
 
     // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+    if (matchArr !== null) {
+      matchArr.forEach(item => {
+        const array = item.split('');
+        const result = array.filter(element => Number.isInteger(Number(element)) === true && element !== ' ');
+        console.log(result.join(''));
+      });
+    } else {
+      // Else print that no number was found
+      console.log("No phone number was found!")
+    }   
 });
