ad6183f3d438fa268bef2108a4465870a519ef99
Jason Metten <metten@Jasons-MacBook-Air.local>
2018-03-21 16:22:47 -0500

    Sprint Completed

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,23 +1,29 @@
 var readline = require('readline');
-
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
-
-
-// This code reads a line at a time from stdin
-
-rl.on('line', function (line) {
-
-    // !!!! IMPLEMENT ME
-
-    // Come up with the phone regex
-
-    // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
-});
+	
+	var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
+	});
+	
+	
+	// This code reads a line at a time from stdin
+	
+	rl.on('line', function (line) {
+	
+	// !!!! IMPLEMENT ME
+	
+	// Come up with the phone regex
+	
+	// Find matches
+	const matches = line.match(/\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/);
+	
+	// If match found, print number with no spaces, parens, or dashes
+	if (matches) {
+	console.log(`${matches[1]}${matches[2]}${matches[3]}`);
+	} else {
+	console.log("No phone number found");
+	}
+	
+	// Else print that no number was found
+	});
