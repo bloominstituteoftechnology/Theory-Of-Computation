02cd57dbb70e3b741c91f0a60b1cdb4bced2ab0d
Cole Phillips <nphillips78@gmail.com>
2018-03-21 09:44:03 -0400

    state machine and links

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -8,13 +8,17 @@ if (args.length != 1) {
 }
 
 const filename = args[0];
-
-// !!!! IMPLEMENT ME
+const regex = /(http|https):\/\/([^\"\']{1,})/g;
+// find anything with either http or https, a colon, 2 slashes(escaped), anything other than quotes, more than one time
 
 // Read file
+fs.readFile(filename, 'utf-8', (err, data) => {
+    if (err) throw err;
+    const dataStr = JSON.stringify(data);
+    const matches = dataStr.match(regex);
+    matches.forEach(match => {
+        console.log(match);
+    })
+})
 
-// Set up regex
 
-// Find matches
-
-// Print all matches
