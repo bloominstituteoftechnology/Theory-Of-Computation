51bc4244bdc3d6e88a390ba662c942af65fd06f9
Pat Collins <padjo@mac.com>
2018-03-21 16:46:25 +0000

    attempt at tel numbers

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,4 +1,11 @@
-var readline = require('readline');
+//https://regexr.com 
+//https://www.regexpal.com
+//https://forum.freecodecamp.org/t/freecodecamp-algorithm-challenge-guide-validate-us-telephone-numbers/16090
+// https://www.youtube.com/watch?v=JB71tFPsm2s
+//https://regexone.com/  -- good turorial
+//https://regexcrossword.com
+
+ var readline = require('readline');
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -14,9 +21,12 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+      regex = "/^(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/";
+      
+   
+   telephoneCheck("555-555-5555");
     // Find matches
-
+    return regex.test(str);
     // If match found, print number with no spaces, parens, or dashes
 
     // Else print that no number was found
