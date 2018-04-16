099c52ce1711a9c55ea4bb5ec41774d003fc2c8e
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-04-16 17:46:44 -0400

    Both regex projects complete, attaching screenshot of state machine

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,17 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const regex = /^[0-9]{10}$/g
 
     // Find matches
-
+    const digits = line.replace(/[^\d]/g, "");
+    const number = regex.test(digits);
     // If match found, print number with no spaces, parens, or dashes
-
+    if (number) {
+      console.log(`The number is ${digits}`);
+    }
     // Else print that no number was found
+    else {
+      console.log("No acceptable number found");
+    }
 });
