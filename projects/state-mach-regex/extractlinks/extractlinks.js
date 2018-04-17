864f1982de8aecad09ed396bd95eeba63069c402
unknown <brcampbell001@outlook.com>
2018-04-17 07:47:07 -0500

    ExtractLinks Completed - UTF bug fixed

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,20 +1,22 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const args = process.argv.slice(2);
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+  console.error("usage: extractlinks inputfile");
+  process.exit(1);
 }
 
 const filename = args[0];
 
-// !!!! IMPLEMENT ME
-
-// Read file
-
-// Set up regex
-
-// Find matches
-
-// Print all matches
+fs.readFile(filename, 'UTF-16le', (err, data) => {
+  if (err) return;
+  // Set up regex
+  const regex = /(htt(p|ps)\:\/\/([\w\W][^"|']+))/g;
+  // Find matches
+  const links = data.match(regex);
+  // Print all matches
+  links.forEach(match => {
+      console.log(match);
+  });
+});
