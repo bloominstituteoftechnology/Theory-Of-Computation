4b28592b276cce05750b35862569aa8e843ebb12
Clara Andrew <candrewwani@gmail.com>
2018-04-17 15:57:26 -0400

    phone regex completed

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,17 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    let phone = /\(?[0-9]\d+\)?\s?-?[0-9]\d+\s?-?[0-9]\d+/g;
 
     // Find matches
+    let matches = line.match(phone);
 
     // If match found, print number with no spaces, parens, or dashes
-
+    if(matches) {
+      let newMatches = matches[0].replace(/[\(\)-\s]/g, '');
+      console.log(newMatches);
+    } else {
+      console.log("No number was found");
+    }
     // Else print that no number was found
 });
