a959974838533650567f6320622aa898cc821c56
Johnathan Justinn <jjustinn85@gmail.com>
2018-04-17 16:38:06 -0400

    Phone number rendered correctly.

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -15,9 +15,20 @@ rl.on('line', function (line) {
 
     // Come up with the phone regex
 
+    let regex = /\(?(\d{3})\)? ?-?(\d{3})-? ?(\d{4})/g
+
     // Find matches
 
+    let match = line.match(regex);
+    match = match.join('').replace(/[^0-9]/gi, '');
+
     // If match found, print number with no spaces, parens, or dashes
+
+    if(match) {
+      console.log(match);
+    } else {
+      console.log("Number not found");
+    }
 
     // Else print that no number was found
 });
