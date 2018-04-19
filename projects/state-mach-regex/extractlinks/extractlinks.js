74c26fb1ad2146835c4b36086c47cd65c6f22c97
Guelmis Cortina <cguelmis@yahoo.com>
2018-04-19 18:20:02 -0400

    print all links using regex

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,23 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
-
+let content;
+let links;
+fs.readFile(filename, function read(err, data) {
+	if (err) throw err;
+	content = data;
+	processFile();
+});
 // Set up regex
+const myRegex = /https?:\/\/.[^\s"']+/gi;
 
 // Find matches
+function processFile() {
+	content = String(content);
+	links = content.match(myRegex);
 
-// Print all matches
+	// Print all matches
+	links.forEach((e) => {
+		console.log(e);
+	});
+}
