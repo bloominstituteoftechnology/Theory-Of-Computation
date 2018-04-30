83be1d57fa2fc98015a2582b4e5f0c99ccf722ad
Mephestys <mephestys@gmail.com>
2018-04-30 14:48:23 -0600

    `extractlinks.js` appears functional - Regex in use could stand to be more accurate, however prints most URLs

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,15 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const loadedFile = fs.readFileSync(filename, {encoding: 'utf8'});
 
 // Set up regex
+const regex = /https?:\/\/[^\\"'>/$.?#\s].[^\\"'>()\s]+/ig;
 
 // Find matches
+const matches = loadedFile.match(regex);
 
 // Print all matches
+for (let link of matches) {
+    console.log(link);
+}
