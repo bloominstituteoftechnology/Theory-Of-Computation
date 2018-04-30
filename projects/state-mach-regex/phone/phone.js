aafea8ed651b973a35f6184e8ee199b9b1ecf9a8
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-30 19:18:29 -0400

    Improved phone.js to fix edge cases

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,20 +14,21 @@ rl.on('line', function (line) {
   // !!!! IMPLEMENT ME
 
   // Come up with the phone regex
-  phone = new RegExp(/(\d{3}).+(\d{3}).(\d{4})/, 'i');
+  let phone = new RegExp(/(\d{3}).*(\d{3}).*(\d{4})/, 'i');
+  let number = new RegExp(/\d/, 'i');
 
   // Find matches
-  if (line.match(phone)) {
-    // console.log(line.match(phone)[0]);
-    line = line.match(phone)[0].split(/[^\d]/);
-    number = line[0].concat(line[1], line[2]);
-    // number = number.concat(number[0], number[1], number[2]);
-    console.log(number);
-  } else {
-    console.log('No valid phone number was found');
-  }
-
   // If match found, print number with no spaces, parens, or dashes
-
+  if (line.match(phone)) {
+    line = line.match(phone)[0]
+      .split('')
+      .filter(c => number.test(c))
+      .join('');
+    console.log(line);
+  }
   // Else print that no number was found
+  else 
+    console.log('No valid phone number was found');
+
+
 });
