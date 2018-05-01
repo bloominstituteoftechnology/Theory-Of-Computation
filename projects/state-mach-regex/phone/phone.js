6513e1edddcc355f917e6aebd525c67ee9c3f22c
Amanda Phillips <amanda@phillipsdev.org>
2018-05-01 18:58:10 -0500

    phone and extractlinks done

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -20,4 +20,22 @@ rl.on('line', function (line) {
     // If match found, print number with no spaces, parens, or dashes
 
     // Else print that no number was found
+    const regex = /\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/;
+    const matches = line.match(regex);
+
+    if(matches === null) {
+      console.log("No number found");
+    } else {
+      console.log(`${matches[1]}${matches[2]}${matches[3]}`);
+    }
+
+
+    // const regExp = /\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})/;
+    // const match = line.match(regExp);
+
+    // if (match !== null) {
+    //   console.log(`${match[1]}${match[2]}${match[3]}`);
+    // } else {
+    //   console.log('Bad phone number Format');
+    // }
 });
