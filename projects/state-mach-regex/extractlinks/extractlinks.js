68e767e0b0afedd990b277166c648bd5c98f5d40
Cliff Kang <inlovewithhim@gmail.com>
2018-05-01 14:32:54 -0700

    added partner code in comments

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -15,7 +15,10 @@ const filename = args[0];
 // let re = /https?:\/\/\S+['">]{2}.+?</g
 // let re = /https?:\/\/\S+['">]{2}\w+?</g
 // let re = /https?:\/\/\S+(?=['"]).+?>\w+</g
+
+// non-stretch
 let re = /https?:\/\/\S+['"]/g
+// let re = /(?<=href=").*?(?=")/g;
 fs.readFile(filename, { encoding: 'utf8' }, (err, data) => {
     if (err) throw err;
     let match = data.match(re);
@@ -23,11 +26,50 @@ fs.readFile(filename, { encoding: 'utf8' }, (err, data) => {
         match = match.map(each => {
             // for stretch
             // console.log(each.slice(0, each.indexOf('>') - 1) + ' ' + each.slice(each.indexOf('>') + 1, each.length - 1));
-            // console.log(each)
+
+            // for non-stretch
             console.log(each.slice(0, each.length - 1))
+            // console.log(each.slice(0, each.length))
         });
     }
 })
+
+// Nikhil's code
+// // Read file
+// let lines = fs.readFileSync(`./${filename}`, 'utf-8').split(/\n|\t/);
+
+// // Set up regex
+// let tagsExp = new RegExp(/<[a].+>.*<\/a>/, 'i');
+// let httpExp = new RegExp(/"http.+?"/);
+// let textExp = new RegExp(/>(\w|\d|\s)+</);
+
+// // Find matches
+// lines = lines
+// // Filter out any lines that don't contain <a> tags
+//     .filter(line => tagsExp.test(line))
+// // Turn each line into just the <a> tags
+//     .map(line => line.match(tagsExp)[ 0 ]);
+
+// let links = lines
+// // Filter out any lines that don't contain httpExp criteria
+//     .filter(line => httpExp.test(line))
+// // Contruct link and text (if exists) for each line
+//     .map(line => {
+//         let link = line.match(httpExp)[ 0 ];
+//         // Remove the quotes from the link
+//         let res = link.slice(1, -1);
+//         // If text match exists, concat the text to the link
+//         if (line.match(textExp)) {
+//             let text = line.match(textExp)[ 0 ];
+//             res += ' || ';
+//             // Remove the surrounding ><'s
+//             res += text.slice(1, -1);
+//         }
+//         return res;
+//     });
+
+// // Print all matches
+// links.forEach(link => console.log(link));
 
 // Set up regex
 
