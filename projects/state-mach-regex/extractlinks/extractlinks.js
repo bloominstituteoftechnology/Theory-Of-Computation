8cb521a848dcb2329b670a7701b991f9904207ae
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-05-01 16:50:01 -0400

    Refactored extractlinks for stretch goal Links are know printing text (if any) alongside link. Only matches links inside <a> tags

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -13,21 +13,37 @@ const filename = args[ 0 ];
 // !!!! IMPLEMENT ME
 
 // Read file
-let lines = fs.readFileSync(`./${filename}`, 'utf-8').split(' ');
+let lines = fs.readFileSync(`./${filename}`, 'utf-8').split(/\n|\t/);
 
 // Set up regex
-let exp = new RegExp(/["']http[s]?.+?["']/, 'i');
+let tagsExp = new RegExp(/<[a].+>.*<\/a>/, 'i');
+let httpExp = new RegExp(/"http.+?"/);
+let textExp = new RegExp(/>(\w|\d|\s)+</);
 
 // Find matches
-lines = lines.filter(line => exp.test(line));
-lines = lines.map(line => {
-    line = line.match(exp)[ 0 ];
-    return line.slice(1, line.length - 1)
+lines = lines
+// Filter out any lines that don't contain <a> tags
+    .filter(line => tagsExp.test(line))
+// Turn each line into just the <a> tags
+    .map(line => line.match(tagsExp)[ 0 ]);
 
-});
+let links = lines
+// Filter out any lines that don't contain httpExp criteria
+    .filter(line => httpExp.test(line))
+// Contruct link and text (if exists) for each line
+    .map(line => {
+        let link = line.match(httpExp)[ 0 ];
+        // Remove the quotes from the link
+        let res = link.slice(1, link.length - 1);
+        // If text match exists, concat the text to the link
+        if (line.match(textExp)) {
+            let text = line.match(textExp)[ 0 ];
+            res += ' || ';
+            // Remove the surrounding ><'s
+            res += text.slice(1, text.length - 1);
+        }
+        return res;
+    });
 
 // Print all matches
-
-lines.forEach(line => {
-    console.log(line);
-});
+links.forEach(link => console.log(link));
