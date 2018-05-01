eef21f49e71f8e9384927780d533ea7a92eb23a3
Dixie Korley <dixiekorley@gmail.com>
2018-05-01 18:16:03 -0400

    Extract links working

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -9,23 +9,27 @@ if (args.length != 1) {
 
 const filename = args[0];
 
-// !!!! IMPLEMENT ME
+//!!!! IMPLEMENT ME
 
 // Read file
-const readFile = fs.readFileSync(filename, {encoding: 'utf8'});
+
+const readFile = fs.readFileSync(filename, 'utf8');
 
 // Set up regex
+
 const regex = /https?:\/\/[^\\'">\s]+?\.[^\\'">\s]+/g;
 
 // Find matches
+
 const matches = readFile.match(regex);
 
 let count = 0;
 
 // Print all matches
+
 for (let url of matches) {
     ++count;
     console.log(url);
 }
 
-console.log("Count: ", count);
+console.log("count: ", count);
