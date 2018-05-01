7c4994095bbe0ffb3a10bae472975de47fdb8309
Dani Tacheny <dani.tacheny@gmail.com>
2018-05-01 16:10:13 -0500

    phone works

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,16 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const regex = /(\d+)[\)-\s]*(\d+)[-\s]*(\d+)/g
 
     // Find matches
+    
+    const number = regex.exec(line);
 
     // If match found, print number with no spaces, parens, or dashes
 
+    if (number) console.log(number[1] + number[2] + number[3]);
+
     // Else print that no number was found
+    else console.log('No number found');
 });
