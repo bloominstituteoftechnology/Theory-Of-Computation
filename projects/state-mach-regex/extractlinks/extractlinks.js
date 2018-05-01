e85e65ef9542d01dad36686996d8b9885b042762
Dixie Korley <dixiekorley@gmail.com>
2018-05-01 16:47:12 -0400

    extractlinks not returning valid output

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,20 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const readFile = fs.readFileSync(filename, {encoding: 'utf8'});
 
 // Set up regex
+const regex = /https?:\/\/[^\\'">\s]+?\.[^\\'">\s]+/g;
 
 // Find matches
+const matches = readFile.match(regex);
+
+let count = 0;
 
 // Print all matches
+for (let url of matches) {
+    ++count;
+    console.log(url);
+}
+
+console.log("Count: ", count);
