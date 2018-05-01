02ce66d9812b86faad3922cdb1e864c0c50015f4
Peter Gray <peter@petergraycreative.com>
2018-05-01 16:23:12 -0500

    Built extract links regex

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -10,14 +10,5 @@ var rl = readline.createInterface({
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
-
-    // !!!! IMPLEMENT ME
-
-    // Come up with the phone regex
-
-    // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+  let r = / /;
 });
