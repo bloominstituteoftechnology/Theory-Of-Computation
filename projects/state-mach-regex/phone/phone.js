369edad8b51b0ecc13759d548181f0e98e8de78e
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-01 17:49:24 -0500

    Phone Regex Update
    - in phone.js:
    - change syntax in line 11 to catch single area code parens

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -8,7 +8,7 @@ var rl = readline.createInterface({
 
 // This code reads a line at a time from stdin
 rl.on('line', (str) => {
-  const regex = /1?(\s|-)?(([(][2-9]\d{2}[)])|([2-9]\d{2}))(\s|-)?\d{3}(\s|-)?\d{4}/;
+  const regex = /1?(\s|-)?(\(?[2-9]\d{2}\)?)(\s|-)?\d{3}(\s|-)?\d{4}/;
   const test = regex.test(str);
   if (test) {
     const regexNum = /[0-9]/;
