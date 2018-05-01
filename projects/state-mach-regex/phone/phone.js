4e0ad5881084d82db4c577aae1dc4bf8fc6231e6
Charlie Sparks <csparks@antioch.edu>
2018-05-01 16:09:23 -0700

    committing for help with phone.js

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,14 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+//const phoneReg = new RegExp("\(?(\d{3})\)?\s*?\-?(\d{3})\-?\s*?(/d{4})"); 
+const phoneReg = new RegExp("^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$");
     // Find matches
-
+const parse = line.match(phoneReg);
     // If match found, print number with no spaces, parens, or dashes
-
+if (!parse){
+  console.log(`The parse failed!`);
+  return
+} else console.log(parse);
     // Else print that no number was found
 });
