f786b33ba9803662681768bd5a93e6f9a73c04d6
Michael Marshal <mikemarshalman@gmail.com>
2018-05-01 15:53:24 -0500

    phone number regexp

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -12,12 +12,17 @@ var rl = readline.createInterface({
 rl.on('line', function (line) {
 
     // !!!! IMPLEMENT ME
-
+    const regExp = /\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})/;
     // Come up with the phone regex
-
+    const match = line.match(regExp);
     // Find matches
 
     // If match found, print number with no spaces, parens, or dashes
-
+    if (match !== null) {
+      console.log(`${match[1]}${match[2]}${match[3]}`);
+    }
     // Else print that no number was found
+    else {
+      console.log('Bad phone number Format');
+    }
 });
