73953beb11c41f3e4858f656b059c18e0d578566
Charlie Sparks <csparks@antioch.edu>
2018-05-01 15:36:45 -0700

    finished first regex assignment; running into an error

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,14 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
-
+const file = fs.readFileSync(filename, "utf8");
 // Set up regex
-
+const regEx = /(https|http):\/\/www\.+?/g;
 // Find matches
-
+const parse = regEx.exec(file);
 // Print all matches
+if (parse === null){
+    console.log(`No matches found`);
+} else {
+    console.log(parse);
+}
