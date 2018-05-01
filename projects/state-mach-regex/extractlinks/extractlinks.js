2c0e24860f64fe662d07d9528f95181e5395d586
unknown <davsoudry@gmail.com>
2018-05-01 15:11:02 -0500

    regex for links working

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,21 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    processFile(data);
+    // console.log('data: ', data);
+})
 
-// Set up regex
+function processFile(data) {
+    // Set up regex
+    const regStr = /(http|https)?:\/\/[a-zA-Z0-9.\/-]+[\.][a-zA-Z0-9.\/-]+/g;
 
-// Find matches
+    // // Find matches
+    const matchedStr = data.match(regStr);
 
-// Print all matches
+    // // Print all matches
+    console.log('matchedStr: ', matchedStr);
+}
