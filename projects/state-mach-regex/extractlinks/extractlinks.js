a0e2d4effe0f41de756ed6a055f4fbd894986a0a
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-05-01 19:10:55 -0400

    Finished extractlinks.js

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,22 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
-
+fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) return console.log(err);
+    printMatches(findMatches(data));
+  });
+  
 // Set up regex
+const search = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]+\.[a-z]+([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
 // Find matches
+const findMatches = data => {
+    return data.match(search);
+}
 
 // Print all matches
+const printMatches = matches => {
+    matches.forEach(link => {
+        console.log(link);
+    })
+}
