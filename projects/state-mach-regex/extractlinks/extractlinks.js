b8d0e2183dba4d00dabea3ff7175f17f319976e3
Igor Yermak <igor.yermak@gmail.com>
2018-05-02 00:27:03 -0400

    Extract Links

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,16 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const file = fs.readFileSync(filename, "utf-8");
 
 // Set up regex
-
+const regex = /href\=\"(http.*?)\"/g;
 // Find matches
+const match = regex.exec(file);
 
 // Print all matches
+if(match === null){
+    console.log(`Error, no matches.`);
+}else {
+    console.log(match);
+}
