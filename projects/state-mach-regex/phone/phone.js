a78d85f9d83d78d59d1189ceaa4fae8ef86d17c6
shobanavv <shobanavv@gmail.com>
2018-05-02 06:30:15 -0700

    Wip - phone number validation

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -12,12 +12,12 @@ var rl = readline.createInterface({
 rl.on('line', function (line) {
 
     // !!!! IMPLEMENT ME
-
+  
     // Come up with the phone regex
-
+  const re = /^[2-9]\d{2}-\d{3}-\d{4}$/g
     // Find matches
-
+  let phoneNumber = line.match(re);
     // If match found, print number with no spaces, parens, or dashes
-
+    console.log(phoneNumber);
     // Else print that no number was found
 });
