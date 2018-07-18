42b099373189772fc8251cdc7781a47a74367f98
Harrison Brock <harrison@harrisonbrock.com>
2018-07-18 14:07:18 -0400

    CODE extract links

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,17 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const reader = fs.readFileSync(filename, 'utf8');
 
 // Set up regex
+const regex = /https?:\/\/[^\\'">\s]+?\.[^\\'">\s]+/g;
 
 // Find matches
-
+const matches = reader.match(regex);
 // Print all matches
+
+for (let url of matches) {
+    console.log(url);
+}
+
+
