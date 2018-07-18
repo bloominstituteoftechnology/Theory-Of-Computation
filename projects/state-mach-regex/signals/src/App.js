ed59c9d3106150d342f74e8d9b2c8ce8c1742ddc
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-18 16:39:53 -0400

    Fixed canvas css and created circles for the different states

--- projects/state-mach-regex/signals/src/App.js
+++ projects/state-mach-regex/signals/src/App.js
@@ -8,8 +8,36 @@ class App extends Component {
     let canvas = document.getElementById('canvas')
     let ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, 700, 800);
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(0, 0, 300, 300);
+
+    this.drawStateMachine(ctx);
+  }
+
+  drawStateMachine(ctx) {
+    // Off Circle 
+    ctx.beginPath();
+    ctx.arc(40, 25, 10, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+
+    // Left Circle
+    ctx.beginPath();
+    ctx.arc(60, 85, 10, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+
+    // Both Circle
+    ctx.beginPath();
+    ctx.arc(160, 85, 10, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    
+    // Right Circle
+    ctx.beginPath();
+    ctx.arc(200, 25, 10, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
   }
 
   render() {
