c49b23370daf32df9105afcc6158a32fce528738
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-18 17:57:34 -0400

    Changed size of canvas and adjusted placement of circles/lines

--- projects/state-mach-regex/signals/src/App.js
+++ projects/state-mach-regex/signals/src/App.js
@@ -12,60 +12,16 @@ class App extends Component {
     ctx.fillRect(0, 0, 300, 300);
 
     this.drawLines(ctx);
+    this.lineText(ctx);
     this.drawCircles(ctx);
     this.circleText(ctx);
-    this.lineText(ctx);
-  }
-
-  drawCircles(ctx) {
-    // Off Circle 
-    ctx.beginPath();
-    ctx.arc(140, 25, 12, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-
-    // Left Circle
-    ctx.beginPath();
-    ctx.arc(60, 110, 12, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-
-    // Both Circle
-    ctx.beginPath();
-    ctx.arc(140, 110, 12, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    
-    // Right Circle
-    ctx.beginPath();
-    ctx.arc(230, 110, 12, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-  }
-
-  circleText(ctx) {
-    ctx.fillStyle = "black";
-    ctx.font="10px ariel";
-
-    // Off Text
-    ctx.fillText("Off", 132, 29);
-
-    // Left Text
-    ctx.fillText("Left", 51, 114);
-
-    // Both Text
-    ctx.fillText("Both", 130, 114);
-
-    // Right Text
-    ctx.fillText("Right", 219, 114);
-    
   }
 
   drawLines(ctx) {
     // Off-To-Left
     ctx.beginPath();
     ctx.moveTo(130, 29);
-    ctx.lineTo(50, 114);
+    ctx.lineTo(20, 114);
     ctx.stroke();
 
     // Off-To-Both
@@ -77,28 +33,28 @@ class App extends Component {
     // Off-To-Right
     ctx.beginPath();
     ctx.moveTo(150, 29);
-    ctx.lineTo(240, 114);
+    ctx.lineTo(280, 114);
     ctx.stroke();
 
     // Left-To-Off
     ctx.beginPath();
-    ctx.moveTo(50, 114);
-    ctx.lineTo(55, 29);
+    ctx.moveTo(20, 114);
+    ctx.lineTo(20, 29);
     ctx.lineTo(130, 29);
     ctx.stroke();
     
     // Left-To-Both
     ctx.beginPath();
-    ctx.moveTo(50, 114);
+    ctx.moveTo(20, 114);
     ctx.lineTo(140, 114);
     ctx.stroke();
 
     // Left-To-Right
     ctx.beginPath();
-    ctx.moveTo(60, 114);
-    ctx.lineTo(60, 130);
-    ctx.lineTo(230, 130);
-    ctx.lineTo(230, 100);
+    ctx.moveTo(20, 114);
+    ctx.lineTo(20, 130);
+    ctx.lineTo(280, 130);
+    ctx.lineTo(280, 100);
     ctx.stroke();
 
     // Both-To-Off
@@ -110,42 +66,47 @@ class App extends Component {
     // Both-To-Left
     ctx.beginPath();
     ctx.moveTo(145, 105);
-    ctx.lineTo(50, 105);
+    ctx.lineTo(20, 105);
     ctx.stroke();
     
     // Both-To-Right
     ctx.beginPath();
     ctx.moveTo(145, 105);
-    ctx.lineTo(240, 105);
+    ctx.lineTo(280, 105);
     ctx.stroke();
     
     // Right-To-Off
     ctx.beginPath();
-    ctx.moveTo(240, 105);
-    ctx.lineTo(240, 29);
+    ctx.moveTo(280, 105);
+    ctx.lineTo(280, 29);
     ctx.lineTo(130, 29);
     ctx.stroke();
     
     // Right-To-Left
     ctx.beginPath();
-    ctx.moveTo(235, 105);
-    ctx.lineTo(235, 140);
-    ctx.lineTo(54, 140);
-    ctx.lineTo(54, 105);
+    ctx.moveTo(285, 105);
+    ctx.lineTo(285, 140);
+    ctx.lineTo(15, 140);
+    ctx.lineTo(15, 105);
     ctx.stroke();
 
     // Right-To-Both
     ctx.beginPath();
-    ctx.moveTo(235, 114);
+    ctx.moveTo(280, 114);
     ctx.lineTo(140, 114);
     ctx.stroke();
   }
 
   lineText(ctx) {
+    ctx.font = "8px ariel";
+    ctx.fillStyle = 'white';
+    
     // Off-To-Left
-
+    ctx.fillText("Handle Down", 70, 65);
+    
     // Off-To-Both
-
+    ctx.fillText("Hazard Button Pressed", 80, 75);
+    
     // Off-To-Right
 
     // Left-To-Off
@@ -165,6 +126,50 @@ class App extends Component {
     // Right-To-Left
 
     // Right-To-Both
+  }
+
+  drawCircles(ctx) {
+    // Off Circle 
+    ctx.beginPath();
+    ctx.arc(140, 25, 12, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+
+    // Left Circle
+    ctx.beginPath();
+    ctx.arc(20, 110, 12, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+
+    // Both Circle
+    ctx.beginPath();
+    ctx.arc(140, 110, 12, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    
+    // Right Circle
+    ctx.beginPath();
+    ctx.arc(280, 110, 12, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+  }
+
+  circleText(ctx) {
+    ctx.fillStyle = "black";
+    ctx.font = "10px ariel";
+
+    // Off Text
+    ctx.fillText("Off", 132, 29);
+
+    // Left Text
+    ctx.fillText("Left", 11, 114);
+
+    // Both Text
+    ctx.fillText("Both", 130, 114);
+
+    // Right Text
+    ctx.fillText("Right", 269, 114);
+    
   }
 
   render() {
