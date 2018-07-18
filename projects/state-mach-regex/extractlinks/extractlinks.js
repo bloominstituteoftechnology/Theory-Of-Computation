af3ccc5d832b659c13261443a34a5e1bbfaa121f
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-07-18 16:38:09 -0400

    working on extracting links/possibly finished

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,44 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+fs.readFile(filename, "utf8", (err, data) => {
+    if (err) {
+        throw err;
+    }
+    // Set up regex
+    let regexToken = /(?:ht|f)tps?:\/\/[-a-zA-Z0-9.]+\.[a-zA-Z]{2,3}(\/[^"<]*)?/g;
+    // Find matches
+    data
+      .split("\n")
+      .map(file => {
+        file.match(regexToken);
+      })
+      .filter(link => link)
+      .reduce((links, current) => {
+        links.concat(current);
+      })
+      // Print all matches
+      .forEach(link => {
+        console.log(link);
+      });
+});
 
-// Set up regex
-let regexToken = /(?:ht|f)tps?:\/\/[-a-zA-Z0-9.]+\.[a-zA-Z]{2,3}(\/[^"<]*)?/g;
-// Find matches
 
-// Print all matches
+
+
+
+
+// fs.readFile(filename, "utf8", (err, data) => {
+//   if (err) throw err;
+
+//   // Set up regex
+//   const re = /https?:\/\/[^"]+(?=")/g;
+
+//   // Find matches
+//   data
+//     .split("\n") // Get lines
+//     .map(datum => datum.match(re)) // Array of arrays (matches for each line)
+//     .filter(link => link) // Remove nulls
+//     .reduce((links, cur) => links.concat(cur)) // Merge results (some lines have multiple matches)
+//     .forEach(link => console.log(link)); // Print links
+// });
