0651516a584bf4963a0744a6b233a6609cea205b
20djm <mostdarryl@gmail.com>
2018-07-18 18:45:06 -0400

    completed regex

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,7 +12,20 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
-
+fs.readFile(filename,'utf8',function(err,data){
+  if(err){
+    console.log(err);
+    return
+  }
+  links = [];
+  words = data.split(" ");
+  for(i = 0; i < words.length; i++){
+    //console.log(words[i]);
+    if(words[i].includes('https://')){
+      console.log(words[i]);
+    }
+  }
+});
 // Set up regex
 
 // Find matches
