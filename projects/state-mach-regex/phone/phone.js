03502e0dee87e457f0e198edcbbfce974654e46d
Natalie <natalieschennum@gmail.com>
2018-07-18 15:46:15 -0700

    Regex Phone Numbers

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,15 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+    let regex = /\(?(\d{3})\)? ?-?(\d{3})-? ?(\d{4})/g
     // Find matches
-
+    let match = line.match(regex);
+    match = match.join('').replace(/[^0-9]/gi, '');
     // If match found, print number with no spaces, parens, or dashes
-
+    if(match) {
+      console.log(match);
+    } else {
+      console.log("Number not found");
+    }
     // Else print that no number was found
 });
