244b44e919b3e2d9352cc5f2075a100aa62c4d12
Justin Hong <jhong2009@gmail.com>
2018-07-19 13:53:00 -0700

    in extractlink.js: set up regex representation of urls and used String.match(regex) to find matches in stackoverflow.html

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -9,13 +9,16 @@ if (args.length != 1) {
 
 const filename = args[0];
 console.log(`file name is ${filename}`);
-// !!!! IMPLEMENT ME
+
+// Read file
 const data = fs.readFileSync("../../../stackoverflow.html");
 console.log(`data ${data}`);
-// Read file
-
+const str = data.toString();
+console.log(`type of data is ${typeof str}`);
 // Set up regex
-
+const regex = /(https|http):\/\/(\w+.)+/g; // regex representation of urls
 // Find matches
-
+const matches = str.match(regex); // holds all matches found with regex in stackoverflow.html
+console.log(`matches ${matches}`);
+console.log(`number of matches is ${matches.length}`);
 // Print all matches
