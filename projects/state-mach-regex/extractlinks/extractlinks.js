e6f09a3513756aa3ece3f78ee096ccba70b4fd25
nickbrenn <nbren@tutanota.com>
2018-07-19 17:55:24 -0400

    Fixed extractlink I think

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -15,14 +15,17 @@ const filename = args[0];
 fs.readFile(filename, "utf8", (err, data) => {
   if (err) throw err;
   // Set up regex
-  const regex = /"https?:\/\/.*\.[a-z]+"/g;
+  const regex = /https?:\/\/[a-z0-9]*\.+[^"']*/gi;
+  // buggier way below
+  // const regex = /https?:\/\/[a-z][^"]+/gi;
   // Find matches
   let matches = data.match(regex);
   // Print all matches
   for (let i = 0; i < matches.length; i++) {
-    // take out extra quotation marks as I go in an inefficient manner
-    // since there is a way I don't know to make them not appear in the first place probably
-    matches[i] = matches[i].replace(/[""]/g, "");
-    console.log(matches[i]);
+    console.log("\n" + matches[i]);
   }
+  console.log(
+    "\n=======================================\nThe number of links is " +
+      matches.length
+  );
 });
