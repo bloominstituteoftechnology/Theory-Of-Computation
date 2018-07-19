d1749258e0d3503ba0e2f7af2f888c59f6f79bb6
Jacob Leon Lyerla <umasour@yahoo.com>
2018-07-19 15:24:19 -0700

    phone regex is working

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,9 +14,14 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+const regex = /^\(?\d{3}\)?(\s|-)?(\d{3})?(\s|-)?(\d{4})$/g
     // Find matches
-
+const match = line.match(regex)
+if(regex.test(line)){
+  console.log(line.replace(/[\s\-\(\)]+/g, ""))
+}else{
+  console.log("No number found.")
+}
     // If match found, print number with no spaces, parens, or dashes
 
     // Else print that no number was found
