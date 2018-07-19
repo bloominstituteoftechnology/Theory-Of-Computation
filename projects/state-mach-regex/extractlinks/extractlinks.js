0a69e10eca9c8945d3796a6158fac73fdc281289
Axhon <axhonruiz@gmail.com>
2018-07-19 13:33:13 -1000

    body of script done, need to fix regex

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,20 +1,36 @@
-const fs = require('fs');
+const fs = require('fs')
+const util = require('util')
 
-const args = process.argv.slice(2);
+const readFile = util.promisify(fs.readFile)
+
+const args = process.argv.slice(2)
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+  console.error('usage: extractlinks inputfile')
+  process.exit(1)
 }
 
-const filename = args[0];
+const filename = args[0]
 
 // !!!! IMPLEMENT ME
 
 // Read file
+async function getTheFile(filename) {
+  return await readFile(filename)
+}
 
 // Set up regex
+const regex = /^<.>$/gm
 
 // Find matches
+function findMatches(str, regex) {
+  return str.match(regex)
+}
 
 // Print all matches
+async function doTheThing() {
+  const fileContents = await getTheFile(filename).then(data => data.toString())
+  console.log(findMatches(fileContents, regex))
+}
+
+doTheThing()
