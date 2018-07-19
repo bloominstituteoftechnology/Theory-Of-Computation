e015e0ebc691de8462c1643387cd1f825b8ed2a3
Justin Hong <jhong2009@gmail.com>
2018-07-19 13:14:41 -0700

    in extractlink.js: used fs.readFileSync() to read from stackoverflow.html

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,16 +1,17 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const args = process.argv.slice(2);
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+	console.error("usage: extractlinks inputfile");
+	process.exit(1);
 }
 
 const filename = args[0];
-
+console.log(`file name is ${filename}`);
 // !!!! IMPLEMENT ME
-
+const data = fs.readFileSync("../../../stackoverflow.html");
+console.log(`data ${data}`);
 // Read file
 
 // Set up regex
