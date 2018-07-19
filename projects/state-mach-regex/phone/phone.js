72a4b8edc9701e64262f5b75b400922d92185f07
SyCoDe <sycode@gmail.com>
2018-07-19 17:04:10 -0600

    completed regex accepts any phone number

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -10,14 +10,11 @@ var rl = readline.createInterface({
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
+  const matches = line.match(/\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/);
 
-    // !!!! IMPLEMENT ME
-
-    // Come up with the phone regex
-
-    // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+  if (matches === null) {
+      console.log("No phone number found");
+  } else {
+      console.log(`Phone # found: ${matches[1]}${matches[2]}${matches[3]}`);
+  }
 });
