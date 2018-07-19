08db42a0b5738c8ae2c18e4f0ffb8a47b1c2c708
Jared Cuffe <jcuffe@gmail.com>
2018-07-19 15:58:07 -0700

    Phone number matcher implemented

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,13 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+    re = /\(?(\d{3})[)\-.]?\s?(\d{3})[\-.]?(\d{4})/g;
     // Find matches
-
+    match = re.exec(line);
     // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+    if (match) {
+      console.log(`Matched: ${match[1]}${match[2]}${match[3]}`);
+    } else {
+      console.log("No matches");
+    }
 });
