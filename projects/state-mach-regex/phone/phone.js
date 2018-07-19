6c8e2f8cb9b3e5eac7b60c62829c86ff33d4480b
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-19 16:29:16 -0400

    Created a match of the phone numbers into an array, prints the output as specified in the README

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -17,8 +17,16 @@ rl.on('line', function (line) {
     const phoneRegex = /\(?(\d{3})\)?\s?-?(\d{3})?\s?-?(\d{4})/;
 
     // Find matches
+    const phoneMatches = line.match(phoneRegex);
 
     // If match found, print number with no spaces, parens, or dashes
-
+    if (phoneMatches) {
+      console.log("Area Code:", phoneMatches[1]);
+      console.log("Prefix:", phoneMatches[2]);
+      console.log("Suffix:", phoneMatches[3]);
+    }
     // Else print that no number was found
+    else {
+      console.log("No number was found.")
+    }
 });
