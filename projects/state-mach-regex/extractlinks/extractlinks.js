af020ce3aefc912334ae5254385f8dae4279ca91
Chris <cjmarsh725@gmail.com>
2018-07-19 14:17:43 -0700

    improved regex

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -15,10 +15,11 @@ const filename = args[0];
 const contents = fs.readFileSync(filename, 'utf8');
 
 // Set up regex
-const reg = /https?/g;
+const reg = /https?:\/\/(www\.)?[\w-]+\.[a-z]+(\/\w+)*([^"']*)/g;
 
 // Find matches
 const matches = contents.match(reg);
 
 // Print all matches
+matches.forEach(url => console.log(url));
 console.log(matches.length);
