c53c44398a2ac0ee4012d5078629f91c1ad88744
Eric Gomez <eric_gomez_1@hotmail.com>
2018-07-19 15:02:22 -0700

    completed phone js file.

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -10,14 +10,12 @@ var rl = readline.createInterface({
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
-
     // Find matches
     const regexp = new RegExp(/\d{10}|\(?(\d{3})\)?(\s|-)?(\d{3})(\s|-)?\d{4}/, 'g')
     // If match found, print number with no spaces, parens, or dashes
     if(regexp.test(line)){
-      line.replace(/[\s\-\(\)]+/g, "");
+      console.log(line.replace(/[\s\-\(\)]+/g, ""));
     } else {
       console.log("No number found.");
     }
-    // Else print that no number was found
 });
