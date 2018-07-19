fc1664df58f56227eaa4cc92757846c4a09795d8
Justin Hong <jhong2009@gmail.com>
2018-07-19 15:36:50 -0700

    in extractlinks.js: added upon regex to ignore closing quotations and brackets

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -15,7 +15,7 @@ const data = fs.readFileSync("../../../stackoverflow.html");
 const str = data.toString();
 // Set up regex
 // \b([-a-zA-Z0-9@:%_\+.~#?&//=]*) refers to a group of chars preceeded by a non word char such as a '/'
-const regex = /(https|http):\/\/([\w\-]{2,256})\b([\w@:%._\+~#?&\/=-]*)/g; // regex representation of urls
+const regex = /(https|http):\/\/([\w\-]{2,256})\b([\w@:%._\+~#?&\/=-]*[^\\'">\s]+)/g; // regex representation of urls
 // Find matches
 const matches = str.match(regex); // holds all matches found with regex in stackoverflow.html
 
