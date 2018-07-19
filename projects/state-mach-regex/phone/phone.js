0603e3db777f9883ef61964c0362e1db1ba942b1
Nedu Robert <nedumrobert@gmail.com>
2018-07-19 22:27:17 +0100

    implemented regex for phone

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,18 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+  const regex = /\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/;
 
     // Find matches
-
+  const match = line.match(regex);
     // If match found, print number with no spaces, parens, or dashes
-
+  if(match !== null) {
+    console.log(`Area Code: ${match[1]}`);
+    console.log(`Prefix: ${match[2]}`);
+    console.log(`Suffix: ${match[3]}`);
+  }
     // Else print that no number was found
+  else {
+    console.log(`number not found!`);
+  }
 });
