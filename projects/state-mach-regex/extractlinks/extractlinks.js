3a15fae88ed89db4ebd3855b2955e5b27f5d74ce
K <kkhaag@gmail.com>
2018-07-19 18:05:22 -0600

    Regex Completed

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -8,13 +8,17 @@ if (args.length != 1) {
 }
 
 const filename = args[0];
-
-// !!!! IMPLEMENT ME
-
 // Read file
-
+file = fs.readFileSync(filename, 'utf8')
 // Set up regex
+const reg  = /https?\:\/\/[^\\'">\s]+/g;
+
+// console.log(file)
 
 // Find matches
-
+const matches = file.match(reg);
 // Print all matches
+
+for (let url of matches){
+    console.log(url)
+}
