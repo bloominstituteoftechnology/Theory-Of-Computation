9ad135150bc9b6172ffddaa18c297000f62fe6f1
Yasin Shuman <email@yasin.io>
2018-07-19 18:12:13 -0700

    extractlinks done

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,20 +1,28 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const args = process.argv.slice(2);
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+  console.error("usage: extractlinks inputfile");
+  process.exit(1);
 }
 
 const filename = args[0];
 
+const regex = /https?:\/\/[^\\'">\s]+?\.[^\\'">\s]+/g;
+
+const file = fs
+  .readFileSync(filename, "utf8")
+  .match(regex)
+  .forEach(link => {
+    console.log(link);
+  });
 // !!!! IMPLEMENT ME
 
-// Read file
+// Read file (line 15)
 
-// Set up regex
+// Set up regex (line 12)
 
-// Find matches
+// Find matches (line 16)
 
-// Print all matches
+// Print all matches (lines 17, 18)
