1b940e015cc08b74d02deac348cd21f62a0679fb
Christina Kopecky <kopecky12112@gmail.com>
2018-08-01 16:42:47 -0700

    regex implemented for phone.js

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -8,13 +8,25 @@ var rl = readline.createInterface({
 
 
 // This code reads a line at a time from stdin
-
 rl.on('line', function (line) {
-
+  const regex = /(\(\d{3}\)\s)|(\d{3}|-|\s)|(\d*)/g;
+  newnumber="";
+  const match = line.match(regex);
+  if(match !== null){
+    const regextwo = /[0-9]/g;
+    const matchtwo = line.match(regextwo);
+    if(matchtwo !== null && matchtwo.length === 10){
+      console.log(matchtwo.join(""));
+    } else {
+      console.error("US Phone Number not found!");
+    }
+  } else{
+    console.error("US Phone Number not found");
+  }
+  
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
     // Find matches
 
     // If match found, print number with no spaces, parens, or dashes
