4b44b6cfef7756dd8c54fac56a0f535b66f7d467
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-08-01 16:00:25 -0700

    fix regex

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -20,7 +20,7 @@ const links = file.match(/(href.+?\>)/g);
 // Find matches
 const urls = []
 links.forEach(link => {
-    let url = link.match(/(http.+?\")/g);
+    let url = link.match(/(https?:\/\/.+?\")/g);
     if (url) {
         url = url[0].slice(0, url[0].length - 1);
         urls.push(url);
