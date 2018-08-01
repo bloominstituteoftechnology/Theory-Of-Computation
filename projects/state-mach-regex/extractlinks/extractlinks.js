3a082a781640608c0f2bdaeb1a15018171e2f93e
A.D.Faris <adfaris@gmail.com>
2018-08-01 18:35:48 -0400

    AD Faris MVP

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const escapeStringRegexp = require('escape-string-regexp');
 
 const args = process.argv.slice(2);
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+  console.error('usage: extractlinks inputfile');
+  process.exit(1);
 }
 
 const filename = args[0];
@@ -12,9 +13,19 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const file = escapeStringRegexp(fs.readFileSync(filename).toString());
 
 // Set up regex
+const links = file.match(/(href.+\>)/g);
 
 // Find matches
+const urls = [];
+links.forEach(link => {
+  const url = link.match(/(http.+?\")/g);
+  if (url) {
+    urls.push(url[0]);
+  }
+});
 
 // Print all matches
+console.log(urls);
--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -3,21 +3,27 @@ var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: false
+  terminal: false,
 });
-
 
 // This code reads a line at a time from stdin
 
-rl.on('line', function (line) {
+rl.on('line', function(line) {
+  // !!!! IMPLEMENT ME
+  // console.log(s2);
+  var s2 = ('' + line).replace(/\D/g, '');
+  if (s2 === '') {
+    console.log('no number was found');
+    return;
+  }
+  var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
+  console.log(`Area-code ${m[1]} \n prefix ${m[2]} \n suffix ${m[3]}`);
 
-    // !!!! IMPLEMENT ME
+  // Come up with the phone regex
 
-    // Come up with the phone regex
+  // Find matches
 
-    // Find matches
+  // If match found, print number with no spaces, parens, or dashes
 
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+  // Else print that no number was found
 });
