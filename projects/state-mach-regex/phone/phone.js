e6ca1720cf7b41e40f600916ff3eb1170409b9cc
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-08-01 14:24:32 -0700

    regex for phone numbers

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -6,18 +6,25 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-
+console.log("running...")
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
-
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const digits = line.replace(/\D/g, '');
+
+    if (digits === '') {
+      console.log('No numbers found');
+    }
 
     // Find matches
+    const areaCode = digits.substring(0,3);
+    const prefix = digits.substring(3,6);
+    const suffix = digits.substring(6,10);
 
     // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+    console.log(`Area code: ${areaCode} \nPrefix: ${prefix} \nSuffix: ${suffix}`);
+    return
 });
