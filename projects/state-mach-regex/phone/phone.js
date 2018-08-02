a37efcd9b37a1b686926d05b1fb16524674a8219
udykas <udykas@gmail.com>
2018-08-02 16:47:58 -0400

    Phone is done

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,18 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const digits = line.replace(/\D/g, '');
+    if (digits === '') {
+      console.log("No phone numbers were found");
+    }
 
     // Find matches
+    const areaCode = digits.substring(0,2);
+    const prefix = digits.substring(3,5);
+    const suffix = digits.substring(6,9);
 
     // If match found, print number with no spaces, parens, or dashes
+    console.log(`Area code: ${areaCode} Prefix: ${prefix} Suffix: ${suffix}`)
 
     // Else print that no number was found
 });
