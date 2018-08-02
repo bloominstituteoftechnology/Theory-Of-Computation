fe695a80471ff40d3c0323471cbf05024f188c66
maribelcuales <maribelcuales@gmail.com>
2018-08-03 07:27:26 +0800

    submit partial regex code for phone.js

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -7,15 +7,30 @@ var rl = readline.createInterface({
 });
 
 
-// This code reads a line at a time from stdin
+// This code reads a line at a time from stdino
+
+// Check for 0 or 1 opening parens
+// Check for three digits
+// Match on 0 or 1 closing parens
+// Match on 0 or more spaces
+// Match on 0 or 1 dashes
+
+const regex = /\(?{\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/;
+
 
 rl.on('line', function (line) {
 
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const matched = line.match(regex);
 
     // Find matches
+    if (!matched) {
+      console.log('Number not found');
+    } else {
+      console.log(`Area code: ${}\nPrefix: ${}\nSuffix: ${}`);
+    }
 
     // If match found, print number with no spaces, parens, or dashes
 
