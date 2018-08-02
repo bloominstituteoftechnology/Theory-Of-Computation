0978a3f22c0deb7368b8ed6a92a8cdcb7534195f
Danny Vail <danny_vail@outlook.com>
2018-08-02 02:12:56 -0400

    Wrote a regex to parse all links - In extractlinks.js: Reads html file passed as command line argument, parses all links and prints them to the console

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,20 +1,18 @@
 const fs = require('fs');
-
 const args = process.argv.slice(2);
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+  console.error('usage: extractlinks inputfile');
+  process.exit(1);
 }
 
 const filename = args[0];
 
-// !!!! IMPLEMENT ME
-
-// Read file
-
-// Set up regex
-
-// Find matches
-
-// Print all matches
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) return console.log('Error: Could not read file');
+  const links = data
+    .match(/\bhref=".*?"/gi)
+    .filter(link => link !== 'href="#"')
+    .map(link => link.replace('href="', '').replace('"', ''))
+    .forEach(link => console.log(link));
+});
