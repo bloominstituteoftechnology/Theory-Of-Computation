7c54aa6194d382ceb9b75274c196492879584518
Bradiowave <divebombwalker@gmail.com>
2018-08-02 13:40:34 -0700

    extractlinks.js returning 177 out of 172 valid links

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,14 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const file = fs.readFileSync(filename).toString();
 
 // Set up regex
+const linkRegex = /http[s]?:[^"']+/img;
 
 // Find matches
+const found = file.match(linkRegex);
 
 // Print all matches
+found.forEach(link => console.log(link));
+console.log(found.length)
