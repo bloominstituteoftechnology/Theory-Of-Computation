93b13f8a6d4808fcc4672e074b00bd5e84d76b7f
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-08-02 18:59:52 -0400

    Validate and format phone numbers

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -20,4 +20,16 @@ rl.on('line', function (line) {
     // If match found, print number with no spaces, parens, or dashes
 
     // Else print that no number was found
+    const regex = /\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}/g;
+
+    const phones = line.match(regex);
+    if(phones){
+      let phone = phones[0].replace(/[\()-\s]/g, '')
+      console.log('Area code: ', phone.slice(0, 3))
+      console.log('Prefix:    ',phone.slice(3, 6))
+      console.log('Suffix:    ',phone.slice(6, 10))
+    }
+    else
+      console.log('Not a valid phone number')
+    
 });
