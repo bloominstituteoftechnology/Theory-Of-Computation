621ed0250dfb1bb6f03f3aaba1c755173022e562
maxwashington <mnwashington18@gmail.com>
2018-08-02 17:10:20 -0500

    Regex completed for extractlinks & phone

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -9,15 +9,28 @@ var rl = readline.createInterface({
 
 // This code reads a line at a time from stdin
 
+console.log("Enter a 10 digit phone number please")
+
 rl.on('line', function (line) {
 
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    let phoneregex = /\(?(\d{3})\)?.?(\d{3}).?(\d{4})/im
 
     // Find matches
+    const number = line.match(phoneregex)
 
     // If match found, print number with no spaces, parens, or dashes
 
+    if(number){
+      console.log("Area Code:", number[1])
+      console.log("Prefix:", number[2])
+      console.log("Suffix:", number[3])
+    }
+
     // Else print that no number was found
+    else{
+      console.log("Sorry number could not be found")
+    }
 });
