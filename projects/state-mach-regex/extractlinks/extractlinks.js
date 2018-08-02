3a474afe23daaa0a2a299b9a98556fc0855131d5
Herbert Hodgson <herberthodgson@hotmail.com>
2018-08-02 18:45:59 -0400

    Write a regex to parse all links from a web page and print them

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const args = process.argv.slice(2);
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+  console.error("usage: extractlinks inputfile");
+  process.exit(1);
 }
 
 const filename = args[0];
@@ -12,9 +12,18 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const readFile = fs.readFile(`./${filename}`, (err, data) => {
+  data = String(data);
+  if (err) {
+    console.log("Unable to read file");
+  }
 
-// Set up regex
+  // Set up regex
+  const regexLinks = new RegExp(/http[s]?.*?(?=")/g);
 
-// Find matches
+  // Find matches
+  const matched = data.match(regexLinks);
 
-// Print all matches
+  // Print all matches
+  console.log(matched);
+});
