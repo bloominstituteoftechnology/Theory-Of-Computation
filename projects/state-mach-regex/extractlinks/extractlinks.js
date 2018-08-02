b13801ec4aac430f3443c28c9e16f5afd6af1d49
JSNavey <jsnavey@gmail.com>
2018-08-02 18:58:09 -0400

    extratct link - minor change and now correctly found 172 links

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -7,7 +7,7 @@ if (args.length != 1) {
     process.exit(1);
 }
 
-const filename = args[0];
+const filename = args[0]; // argv[0] will be the path to node itself, and the second item (argv[1]) will be the path to your script code.
 
 // !!!! IMPLEMENT ME
 
@@ -17,7 +17,8 @@ fs.readFile(filename, 'utf8', (err, links) => {
         return console.log(err)
     }
     // Set up regex
-    const regex = /https?:\/\/[^\\'">\s]+/ig;
+    // between [ ] exclusive these charecters which invalid in url, match 1 or more
+    const regex = /https?:\/\/[^\\'"<>\s]+?\.[^\\'"<>\s]+/ig;
     // Find matches
     const allLinks = links.match(regex);
     // Print all matches
