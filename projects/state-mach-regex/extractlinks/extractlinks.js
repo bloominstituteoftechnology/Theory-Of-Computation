612b88b093d8a124764fb4a6bc77557ebe98a6c0
Mister-Corn <code@ronaldlibago.com>
2018-08-02 14:40:44 -0700

    14:40 PDT 2-Aug: Have an OK implementation of `extractlinks.js`

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -10,11 +10,25 @@ if (args.length != 1) {
 const filename = args[0];
 
 // !!!! IMPLEMENT ME
+function main(filename) {
+    // Read file
+    let fileTxt;
+    try {
+        fileTxt = String(fs.readFileSync(filename));
+    } catch (err) {
+        process.stdout.write('ERROR: File could not be opened. Please double check path.\n');
+        return;
+    }
+    // Set up regex
+    const linkRegex = /https?:\/\/([^\/"']+)\.([^\/"'\W]+)\/?([^"']+)/gi;
+    // Find matches
+    const matches = fileTxt.match(linkRegex);
+    // Print all matches
+    for (const url of matches) {
+        process.stdout.write(`${url}\n`);
+    }
+    process.stdout.write(`---\nNumber of matches: ${matches.length}\n`);
+    return;
+}
 
-// Read file
-
-// Set up regex
-
-// Find matches
-
-// Print all matches
+main(filename);
