c0ac78403ab1f1eb249720284c7c2a219ce157c4
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-08-02 16:02:22 -0500

    complete phone regex

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -9,15 +9,21 @@ var rl = readline.createInterface({
 
 // This code reads a line at a time from stdin
 
+// console.log('test')
 rl.on('line', function (line) {
-
+  console.log('test')
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+    let phoneRegex = /[(]?(\d{3})[)]?.?(\d{3}).?(\d{4})/im
     // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+     // If match found, print number with no spaces, parens, or dashes
+    const found = line.match(phoneRegex)
+    if (found) {
+      console.log('Area code: ', found[1])
+      console.log('Prefix: ', found[2])
+      console.log('Suffix: ', found[3])
+    }
+     // Else print that no number was found
+    else console.log('no number was found')
 });
