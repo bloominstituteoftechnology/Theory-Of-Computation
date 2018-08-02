29f90992877f41a890d377f93c0515bdadf39f9f
AmyShackles <amyshackles@gmail.com>
2018-08-02 11:42:53 -0700

    Phone regex works
    - Found a bug in my original code that enabled a user to input additional characters at the end of a valid phone number.  That no longer constitutes a phone number.

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -9,17 +9,17 @@ var rl = readline.createInterface({
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
-  let regEx = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})/
-  let brokenUp = line.match(regEx)
+  let regEx = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  let brokenUp = line.match(regEx);
 
   if (brokenUp === null) {
-    console.log('No number was found.')
+    console.log('No number was found.');
   }
   else if (brokenUp != null) {
-    let areaCode = brokenUp[1]
-    let prefix = brokenUp[2]
-    let suffix = brokenUp[3]
-    console.log(`Area code: ${areaCode}\nPrefix: ${prefix}\nSuffix: ${suffix}`)
+    let areaCode = brokenUp[1];
+    let prefix = brokenUp[2];
+    let suffix = brokenUp[3];
+    console.log(`Area code: ${areaCode}\nPrefix: ${prefix}\nSuffix: ${suffix}`);
   }
 });
 
