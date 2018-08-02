054f15eedfccfbaf1b1adeebf2c502a5be2026ff
Danny Vail <danny_vail@outlook.com>
2018-08-02 02:45:22 -0400

    Wrote a regex that accepts a phone number - In phone.js: Reads line from terminal and determines if it is a phone number. Phone numbers are printed back to terminal as area code, prefix and suffix. Invalid phone number formats trigger an error.

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -6,18 +6,18 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-
 // This code reads a line at a time from stdin
-
-rl.on('line', function (line) {
-
-    // !!!! IMPLEMENT ME
-
-    // Come up with the phone regex
-
-    // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+rl.on('line', function(line) {
+  if (line.match(/\(*?\d{3}[)\- ]*?\d{3}[\- ]*?\d{4}/g)) {
+    const phoneNumber = line.replace(/[- \()]/g, '');
+    console.log(`\nArea code: ${
+      phoneNumber.substring(0, 3)
+    }\nPrefix: ${
+      phoneNumber.substring(3, 6)
+    }\nSuffix: ${
+      phoneNumber.substring(6)
+    }\n`);
+  } else {
+    console.log('Error: Invalid phone number format\n');
+  }
 });
