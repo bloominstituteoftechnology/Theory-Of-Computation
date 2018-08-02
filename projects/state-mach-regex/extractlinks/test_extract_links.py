3771b7cfa6d655c809dec113dadbde261e221ae3
Sean Chen <skypemaster007@gmail.com>
2018-08-02 17:16:55 -0500

    Remove code comparing length of links and answers

--- projects/state-mach-regex/extractlinks/test_extract_links.py
+++ projects/state-mach-regex/extractlinks/test_extract_links.py
@@ -10,10 +10,6 @@ if __name__ == '__main__':
   with open('./links.txt') as f:
     links = json.load(f)
 
-  if len(links) != len(answers):
-    print('There should be %s links but you found %s' %(len(answers), len(links)))
-    sys.exit(0)
-
   for i in range(len(answers)):
     if links[i] != answers[i]:
       print('Mismatched link. Expected: \n%s\nbut got: \n%s' %(answers[i], links[i]))
