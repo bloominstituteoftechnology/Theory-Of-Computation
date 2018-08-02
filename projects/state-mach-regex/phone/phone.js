8ad1f744acc332c7391e01d82e4f68e148d40ae1
Mister-Corn <code@ronaldlibago.com>
2018-08-02 15:16:40 -0700

    15:16 PDT 2-Aug: Working implementation of `phone.js`

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -12,12 +12,15 @@ var rl = readline.createInterface({
 rl.on('line', function (line) {
 
     // !!!! IMPLEMENT ME
-
     // Come up with the phone regex
-
+    const phoneRegex = /\(?(\d{3})[)-]?\s?(\d{3})[-\s]?(\d{4})/
     // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+    const result = line.match(phoneRegex);
+    if (result) {
+      // If match found, print number with no spaces, parens, or dashes
+      process.stdout.write(`Area code: ${result[1]}\nPrefix: ${result[2]}\nSuffix: ${result[3]}\n`)
+    } else {
+      // Else print that no number was found
+      console.log('Invalid phone number.');
+    }
 });
