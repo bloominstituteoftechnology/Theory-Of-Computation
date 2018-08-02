5e995ddd679cdfccb10fa7f853394a062d5618ef
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-02 18:08:06 -0400

    extractedlink.js

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,4 +1,4 @@
- const fs = require('fs');
+const fs = require('fs');
 
 const args = process.argv.slice(2);
 
@@ -12,9 +12,16 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const readfile = rs.readfile(`./${filename}`, (err, data) => {
+    data = String(data)
 
 // Set up regex
+const findLinks = new RegExp(/http[s]?.*?(?=")/g);
 
 // Find matches
 
+const matches = data.match(findLinks)
+
 // Print all matches
+    return matches;
+})
