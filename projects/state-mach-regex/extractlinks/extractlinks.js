eff157ff80f1688b9bcd513138efec6b4ac90b0a
William VanDolah <william.vandolah@gmail.com>
2018-08-02 17:27:49 -0500

    able to get all links from html file

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -9,12 +9,22 @@ if (args.length != 1) {
 
 const filename = args[0];
 
+// console.log(args)
+
 // !!!! IMPLEMENT ME
 
 // Read file
-
+file = fs.readFileSync(filename, 'utf8')
+// console.log(file)
 // Set up regex
-
+const regex = /["']https?:\/\/[^'"]*?\.[^'"]*?["']/g;
 // Find matches
-
+const output = file.match(regex)
 // Print all matches
+console.log(output)
+fs.writeFile('./links.txt', output, (ferr) => {
+    if (ferr) {
+      // console.log('Error writing secret key to confing file: ', ferr.message)
+      return;
+    }
+  });
