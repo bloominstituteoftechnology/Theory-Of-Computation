62e43f463489d97e3d2bd8504d884d773250a05a
maribelcuales <maribelcuales@gmail.com>
2018-08-03 06:33:56 +0800

    added regex (code not yet final) to extractlink.js

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,3 +1,5 @@
+// import ('stackoverflow.html');
+
 const fs = require('fs');
 
 const args = process.argv.slice(2);
@@ -12,9 +14,26 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const readFile = fs.readFileSync(filename).toString();
 
 // Set up regex
+const regex = /(http|https+)(.+)(\"{1}|\'{1})/g;
+const matchedLinks = readFile.match(regex);
+
+// (http|https+)(.+)('{1}|"{1})
+// /(http|https+)(.+)("{1})/g;
+// /(http|https+)(.+(?="))/g;
 
 // Find matches
+const urls = []
 
+matchedLinks.forEach(link => {
+    let url = link.match(regex);
+    if (url) {
+        url = url[0].slice(0, url[0].length - 1);
+        urls.push(url);
+    }
+})
+ 
 // Print all matches
+console.log(urls);
