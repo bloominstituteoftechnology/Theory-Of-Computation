6582896cd791e189e38165c69e48d9dfb427a800
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-08-02 17:32:37 -0500

    extractlinks wip

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,15 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const data = fs.readFileSync(filename, 'utf8')
 
 // Set up regex
-
+const urlRegex = /http[s]?\:\/\/\w+\.?/g 
 // Find matches
-
+const found = data.match(urlRegex)
 // Print all matches
+
+// console.log(found.length)
+
+const foundModified = found.map(item => item.substring(1, url.length))
+console.log(foundModified)
