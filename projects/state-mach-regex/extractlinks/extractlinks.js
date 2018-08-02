8ba600b7aafe03ea15631a4954c1683ee3fc9d42
derrick-mei <derrick.mei.168@gmail.com>
2018-08-02 16:03:43 -0500

    set up system to read document, and console links and links count

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,19 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const data = fs.readFileSync('./stackoverflow.html', 'UTF8');
+// console.log(data)
+
 
 // Set up regex
+//const regex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+
 
 // Find matches
+const links = data.match(regex)
+const numLinks = links.length
+
 
 // Print all matches
+links.forEach( item => console.log(item))
+console.log("Number of Links: ", numLinks)
