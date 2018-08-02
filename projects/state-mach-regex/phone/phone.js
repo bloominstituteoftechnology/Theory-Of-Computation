61e796b48fd386b6185504ff6c9c89e60c5f59df
NandoTheessen <nando.theessen@gmail.com>
2018-08-02 22:49:53 +0200

    Working phone nr example

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,21 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+    // regex for a german mobile phone number
+    // 4 digits followed by additional 8 numbers 
+    // with restrictions for the first 4 numbers
+    const regex = /\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/
     // Find matches
-
+    // const strippedNumber = line.replace(/\D+/g, '')
+    const outcome = line.match(regex)
     // If match found, print number with no spaces, parens, or dashes
-
+    if(outcome){
+      console.log('Area Code: ', outcome[1]);
+      console.log('Prefix: ', outcome[2]);
+      console.log('Suffix: ', outcome[3]);
+    }
     // Else print that no number was found
+    else{
+      console.log('No number was found', outcome);
+    }
 });
