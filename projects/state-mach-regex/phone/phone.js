95767fd97e960d7d092d18d02a0f786e2dd96d3e
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-08-03 05:43:18 +0800

    Wrote regex that accepted a phone number and returned with the number broken down into area code, prefix, and suffix.

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,6 +1,6 @@
-var readline = require('readline');
+const readline = require('readline');
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
@@ -14,10 +14,19 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const digits = line.replace(/\D/g, "");
+
+    if (digits === "") {
+      console.log("Error: found no numbers");
+    }
 
     // Find matches
+    const areaCode = digits.substring(0, 3);
+    const prefix = digits.substring(3, 6);
+    const suffix = digits.substring(6, 10);
 
     // If match found, print number with no spaces, parens, or dashes
-
+    console.log(`Are Code: ${areaCode} \nPrefix: ${prefix} \nSuffix: ${suffix}`);
+    return
     // Else print that no number was found
 });
