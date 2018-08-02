af216c73408150a050ff2bd749b697c728616799
Bradiowave <divebombwalker@gmail.com>
2018-08-02 14:20:32 -0700

    phone.js complete

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,19 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const phoneRegex = /(?:.*)([0-9]{3})(?:.*?)([0-9]{3})(?:.*?)([0-9]{4})/
 
     // Find matches
+    const found = line.toString().match(phoneRegex);
 
     // If match found, print number with no spaces, parens, or dashes
-
+    if (found) {
+      console.log('Area code: ', found[1])
+      console.log('Area code: ', found[2])
+      console.log('Area code: ', found[3])
+    }
     // Else print that no number was found
+    else {
+      console.log('No phone number found')
+    }
 });
