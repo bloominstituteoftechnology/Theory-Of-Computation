6a4050f5b8840a0291157c79c9bf67137a10508f
AmyShackles <amyshackles@gmail.com>
2018-08-02 14:06:26 -0700

    Commented regex for extraction

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -14,13 +14,24 @@ fs.readFile(filename, 'utf8', (err, data) => {
     if (err) throw err;
 
     const regex = /https?:\/\/(\w)+\-?(\w)+\.(\w*\/?\w*\.?\??\=?\-?&?;?)*/g
+    // http, zero or 1 s, a colon, two forward slashes,
+    // one or more instances of an alphanumeric character
+    // zero or one instance of hyphen
+    // one or more instances of an alphanumeric character
+    // a period
+    // Capture group start {
+    // 0 or more alphanumerical characters
+    // zero or one periods
+    // zero or one question marks
+    // zero or one equal sign
+    // zero or one hyphen
+    // zero or one ampersand
+    // zero or one semicolon
+    // } capture group ends
+    // zero or more instances of the capture group
 
     const links = data.match(regex)
-    let variable = new Set()
-    for (let i = 0; i < links.length; i++) {
-        // variable.add(links[i])
-        variable.add(links[i])
-    }
+
     if (links === null) {
         console.log('Cannot retrieve links')
     } else {
