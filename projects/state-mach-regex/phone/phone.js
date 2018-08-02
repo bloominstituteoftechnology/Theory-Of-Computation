2f03261193018798b1be2c2c4774c84aafaf754c
David Loveday <davemech@yahoo.com>
2018-08-02 13:44:19 -0700

    regex for phone numbers

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,20 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
+    const dig = line.replace(/\D/g, '');
+
+    if (digits === '') {
+      console.log('No number was found');
+    }
 
     // Find matches
+    const areaCode = digits.substring(0,3);
+    const prefix = digits.substring(3,6);
+    const suffix = digits.substring(6,10);
 
     // If match found, print number with no spaces, parens, or dashes
+    console.log(`Area code: ${areaCode} \nPrefix: ${prefix} \nSuffix: ${suffix}`);
+    return
 
     // Else print that no number was found
 });
