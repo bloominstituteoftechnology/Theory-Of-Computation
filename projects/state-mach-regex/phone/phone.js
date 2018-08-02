99b0d11c79486e2ead633bdfdd3b6081402acf86
William VanDolah <william.vandolah@gmail.com>
2018-08-02 18:27:19 -0500

    added phone

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -14,10 +14,16 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+    const regex = /\(?(\d{3})[^\d]*(\d{3})[^\d]*(\d{4})/
+    // const regex = /\(?(\d{3})\)?\s*-?\s*(\d{3})/
     // Find matches
-
+    const outcome = line.match(regex)
     // If match found, print number with no spaces, parens, or dashes
-
+    if(outcome){
+      console.log(`Area code: ${outcome[1]}\nPrefix: ${outcome[2]}\nSuffix: ${outcome[3]}`)
+      // console.log(outcome)
+    }else{
+      console.log('no number found')
+    }
     // Else print that no number was found
 });
