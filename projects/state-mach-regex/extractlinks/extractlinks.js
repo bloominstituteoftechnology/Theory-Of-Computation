85a7d18b1f63edc68113f40e42540529685d6763
William VanDolah <william.vandolah@gmail.com>
2018-08-03 16:05:42 -0500

    testing ssh

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); 
 
 if (args.length != 1) {
     console.error("usage: extractlinks inputfile");
