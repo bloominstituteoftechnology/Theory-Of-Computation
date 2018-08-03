0d43f64f79f01cf7fe0a427e6ab068d5a92680c0
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-08-02 20:17:59 -0400

    Completed extractlinks and phone regex challenges.

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,23 +1,36 @@
-var readline = require('readline');
+const readline = require('readline');
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
 
-
 // This code reads a line at a time from stdin
+
+// Match on 0 or 1 opening parens
+// Match on three digits 
+// Match on 0 or 1 closing parens
+// Match on 0 or more spaces 
+// Match on 0 or 1 dashes 
+// Match on 3 digits 
+// Match on 0 or more spaces 
+// Match on 0 or 1 dashes 
+// Match on 4 digits 
+
+const regex = /\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/; 
 
 rl.on('line', function (line) {
 
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
-    // Find matches
+    const matches = line.match(regex); 
 
     // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+    if (!matches) {
+      console.log('No number found.')
+    } else {
+      console.log(`Area code: ${matches[1]}\nPrefix: ${matches[2]}\nSuffix: ${matches[3]}`)
+    }
 });
