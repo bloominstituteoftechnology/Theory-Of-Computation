08cdee6db706b3f7ac8b9cc330a825140aa05a77
Lavell Burton <taylormade800@gmail.com>
2018-08-08 11:52:20 -0700

    added comments to phone and extractlinks and updated readme for objectives

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,11 +12,19 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const fileData = fs.readFileSync(filename, { encoding: 'utf8' });
 
 // Set up regex
+const regex = /https?:\/\/[^\\’”<>\s]+?\.[^\\’”<>\s]+/g;
 
 // Find matches
+const matches = fileData.match(regex);
+
+
+for (let url of matches) {
+    console.log(url);
+}
+
 
 // Print all matches
-
-// making pull request
+console.log(matches.length)
--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -7,17 +7,22 @@ var rl = readline.createInterface({
 });
 
 
+  // Come up with the phone regex
+const regex = /\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/;
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
 
-    // !!!! IMPLEMENT ME
+  // Come up with the phone regex
+  // Find matches
+  const matches = line.match(regex);
 
-    // Come up with the phone regex
 
-    // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
+  // If match found, print number with no spaces, parens, or dashes
+  if(!matches) {
+    console.log('No number found');
+  } else {
     // Else print that no number was found
+    console.log('Area code: ${mathes[1]}\nPrefix: ${matches[2]}\nSuffix: ${matches[3]}');
+  }
 });
