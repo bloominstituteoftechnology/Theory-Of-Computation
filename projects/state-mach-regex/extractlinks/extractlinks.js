99357988b866acce7d92791d5012c6ab47131842
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-08-08 13:59:29 -0700

    MBP REDO
    -adds simplified and redone turn-signal state machine diagram REDO.jpg

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -15,7 +15,15 @@ const filename = args[0];
 const file = fs.readFileSync(filename).toString();
 
 // Set up regex
-let regex = /(https:)\S+/g;
+// let regex = /(https:)\S+/g; // my implementation, returns 173 links, should be 172
+let regex = /https?:\/\/[^\\'"<>\s]+?\.[^\\'"<>\s]+/g; // correct implementation, returns 172 links
+/* 
+https? = Match on http or https
+:\/\/ = match on "://" using \ to escape the backslashes
+[^\\'"<>\s]+? = match 1 or more on any characters NOT in this set, lazily
+\. = match on the period
+[^\\'"<>\s]+ = match 1 or more on any characters NOT in this set, greedily
+*/
 
 // Find matches
 const links = file.match(regex);
--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,33 +1,36 @@
-var readline = require('readline');
+const readline = require('readline');
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
 
+// match on 0 or 1 opening parens
+// match on three digits
+// match on 0 or 1 closing parens
+// match on 0 or more spaces
+// match on 3 digits
+// match on 0 or more spaces
+// match on 0 or 1 dashes
+// match on 4 digits
+
+const phoneRegex = /\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/;
+// const phoneRegex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/gm;
 
 // This code reads a line at a time from stdin
-
 rl.on('line', function (line) {
-
     // !!!! IMPLEMENT ME
-    const phoneNums = [
-      '555-123-4567',
-      '5551234567',
-      '(555) 123-4567',
-      '555 123 4567'
-    ];
 
     // Come up with the phone regex
-    const phoneRegex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/gm;
-
     // Find matches
-    const found = phoneNums.match(phoneRegex);
+    const matches = line.match(phoneRegex);
 
     // If match found, print number with no spaces, parens, or dashes
-    console.log("area code: ", found[1]);
-    console.log("prefix: ", found[2]);
-    console.log("suffix: ", found[3]);
+    if (matches) {
+      console.log(`Area Code: ${matches[1]}\nPrefix: ${matches[2]}\nSuffix: ${matches[3]}`);
     // Else print that no number was found
+    } else {
+      console.log('no number found');
+    };
 });
