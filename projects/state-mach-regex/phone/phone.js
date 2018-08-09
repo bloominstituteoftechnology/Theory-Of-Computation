c743370dbadf8fda344cdb1df74fe662ccaa210a
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-08-09 15:52:10 -0700

    completed a comment I had started typing

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -25,7 +25,7 @@ const rl = readline.createInterface({
 const phoneRegex = /^[(]{0,1}([0-9]{3})[)]{0,1}[-\s\.]{0,1}([0-9]{3})[-\s\.]{0,1}([0-9]{4})$/;
 // -much like in the solution above, my regex needed the 'g' and 'm' flag on RegExr and yet 
 // if I included it in the regex here, it causes the variable 'matches' to be undefined.
-// -what wasn't working last week: besides the flags at the end, 
+// -what wasn't working last week: besides the flags at the end, I needed to group the digits
 
 // This code reads a line at a time from stdin
 rl.on('line', function (line) {
