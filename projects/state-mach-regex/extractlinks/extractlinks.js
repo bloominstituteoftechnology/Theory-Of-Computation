1f49b037f0817d4d7e6acbcee14a02c9d0cd06f4
Bhavik Ravani <ravanibhavik@gmail.com>
2018-09-19 23:52:14 -0400

    Completed URL matching for RegExp.

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,30 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const lines = fs.readFileSync(filename, 'utf8').split("\n");
+let rawData = '';
 
 // Set up regex
+lines.forEach(line => {
+    rawData += line.trim();
+    rawData += '\n';
+});
 
+rawData = rawData.trim();
+// console.log(rawData)
 // Find matches
 
-// Print all matches
+let pattern = new RegExp('((([A-Za-z]{3,9}:(?:\/\/)?)' +
+               '(?:[\-;:&=\+\$,\w]+@)' + 
+               '?[A-Za-z0-9\.\-]+|' +
+               '(?:www\.|[\-;:&=\+\$,\w]+@)' +
+               '[A-Za-z0-9\.\-]+)' + 
+               '((?:\/[\+~%\/\.\w\-_]*)?\??' +
+               '(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)', 'g');
+// let pattern = /https?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/g
+// let urls = pattern.exec(rawData)
+let urls = rawData.match(pattern);
+
+// Print all matches(
+urls.forEach(url => console.log(url))
+
