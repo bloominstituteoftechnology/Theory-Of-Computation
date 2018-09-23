e3c43f0226c853ec5014192573139f8b438351ae
Bhavik Ravani <ravanibhavik@gmail.com>
2018-09-23 00:51:51 -0400

    Completed Phone Regex.

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -6,7 +6,6 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
@@ -14,10 +13,19 @@ rl.on('line', function (line) {
     // !!!! IMPLEMENT ME
 
     // Come up with the phone regex
-
+    let phoneRegex = /\(?(\d{3})\)?-?(\d{3})-?(\d{4})/g
     // Find matches
-
+    let matchedPhone = phoneRegex.exec(line)
+    
     // If match found, print number with no spaces, parens, or dashes
-
+    if (matchedPhone) {
+      // console.log(matchedPhone.length);
+      console.log(`Area Code: ${matchedPhone[1]}`)
+      console.log(`Prefix: ${matchedPhone[2]}`)
+      console.log(`Suffix: ${matchedPhone[3]}`)
+    }
     // Else print that no number was found
+    else {
+      console.log("No number found.")
+    }
 });
