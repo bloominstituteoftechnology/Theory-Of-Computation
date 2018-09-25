55b5fe57d60a50268b3229f554d357aa9bf7e996
et-code-home <elissa.thomas@lambdaschool.com>
2018-09-25 14:43:12 -0500

    final phone clean-up, add TODOs

--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -3,24 +3,23 @@ import re # module for processing regular expressions https://docs.python.org/3/
 # Initial prompt to user
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
-# Define your regex
-regex = '\+?([\d]{0,3})[-\s]?\(?(\d{3})\)?\s?\-?(\d{3})\s?\-?(\d{4})'
+# TODO Define your regex
+
 
 while line != "exit":
-    # !!!! IMPLEMENT ME
+    # TODO Find matches
 
-    # Find matches
-    matches = re.search( regex, line )
     
-    # If no match found, print that no number was found
-    if not matches:
-        print( 'Not a valid phone number :(' )
+    # TODO If no match found, print that no number was found
+   
+   
     
-    # Else, break number up into area code, prefix, and suffic
+    # TODO Else, break number up into area code, prefix, and suffic
+    
+    
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
-    else:
-        print('Country code: %s \t Area code: %s \t Prefix: %s \t Suffix: %s' % (matches[1], matches[2], matches[3], matches[4]) )
+    
     
     # Done validating, read in a new line
     line = input("Enter a phone number to validate or 'exit' when done. ")
