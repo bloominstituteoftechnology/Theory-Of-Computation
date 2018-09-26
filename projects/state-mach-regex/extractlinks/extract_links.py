0b6a36b4d7b5d37200603178b44882bedd841c08
et-code-home <elissa.thomas@lambdaschool.com>
2018-09-25 23:06:41 -0500

    Adding python solutions

--- projects/state-mach-regex/extractlinks/extract_links.py
+++ projects/state-mach-regex/extractlinks/extract_links.py
@@ -11,27 +11,34 @@ if __name__ == '__main__':
 filename = sys.argv[1]
 
 # TODO Read HTML file
-
+filedata = ''
+with open(filename) as file:
+    filedata = file.read()
+file.close()
 
 # TODO Set up regex
-
+regex = 'https?:\/\/[^\"\ \<\>\{\}\|\\\^\~\[\]\']+?\.[^\"\ \<\>\{\}\|\\\^\~\[\]\']+'
 
 # TODO Find links using regex, save in list called 'matches'
-
+matches = re.findall( regex, filedata )
 
 # Check matches, print results
 # TODO Read in links from answers.txt (hint...this is a CSV file), 
 # save in list called 'answer_data'
+with open("answers.txt") as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+  for row in csv_reader:
+    answer_data = row
 
 
 # Compare answers with matches found using regex, print out any mismatches
 # UNCOMMENT BELOW WHEN READY TO CHECK IF YOUR REGEX IS FINDING ALL THE LINKS
-# result = "All links matched!"
-# if len( matches ) != len( answer_data ):
-#   result = "Your regex found %i matches. There should be %i matches" %(len( matches ), len( answer_data ) )
-# else:
-#   for i in range( len(answer_data) ):
-#     if( matches[i] != answer_data[i] ):
-#       result = "Mismatched link. Got %s but expected %s" % ( matches[i], answer_data[i] )
-#       break
-# print( result )
+result = "All links matched!"
+if len( matches ) != len( answer_data ):
+  result = "Your regex found %i matches. There should be %i matches" %(len( matches ), len( answer_data ) )
+else:
+  for i in range( len(answer_data) ):
+    if( matches[i] != answer_data[i] ):
+      result = "Mismatched link. Got %s but expected %s" % ( matches[i], answer_data[i] )
+      break
+print( result )
--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -4,17 +4,19 @@ import re # module for processing regular expressions https://docs.python.org/3/
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # TODO Define your regex
-
+regex = '\+?([\d]{0,3})[-\s]?\(?(\d{3})\)?\s?\-?(\d{3})\s?\-?(\d{4})'
 
 while line != "exit":
     # TODO Find matches
-
+    matches = re.search( regex, line )
     
     # TODO If no match found, print that no number was found
-   
-   
+    if not matches:
+        print( 'Not a valid phone number :(' ) 
     
     # TODO Else, break number up into area code, prefix, and suffic
+    else:
+        print('Country code: %s \t Area code: %s \t Prefix: %s \t Suffix: %s' % (matches[1], matches[2], matches[3], matches[4]) )
     
     
     # As a stretch goal, you can modify your regex to search for country codes
