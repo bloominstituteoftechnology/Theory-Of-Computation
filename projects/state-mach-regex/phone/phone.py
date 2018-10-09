12983e0cb07b13e832b5583505892b0efc32aeb6
Alec <afroenygam@gmail.com>
2018-10-09 19:40:04 -0400

    phone.py mvp updated, still working on adding country code funtionality

--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -9,15 +9,17 @@ regex = '\+?([\d]{0,3})[-\s]?\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})'
 
 while line != "exit":
     # TODO Find matches
-    re.findall( regex, line)
+    matches = re.search( regex, line)
 
     
     # TODO If no match found, print that no number was found
-   
+    if not matches:
+       print("No Number was found")
    
     
     # TODO Else, break number up into area code, prefix, and suffic
-    
+    else:
+        print('Area code: %s\nPrefix: %s\nSuffix: %s\n' % (matches[2], matches[3], matches[4]))
     
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
