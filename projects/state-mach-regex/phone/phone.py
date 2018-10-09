f7515fa36404e029458d483cfbeacc15db4f2811
Simon <Simon_Freelance@hotmail.com>
2018-10-09 18:20:42 -0400

    Matched phone number with outputting desired groups

--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -4,17 +4,24 @@ import re # module for processing regular expressions https://docs.python.org/3/
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # TODO Define your regex
-
+phone_regex = re.compile("\(?\d{3}\)?[-\s]*\d{3}[-\s]*\d{4}")
 
 while line != "exit":
     # TODO Find matches
-
-    
+    phone_nums = re.findall(phone_regex, line)
     # TODO If no match found, print that no number was found
-   
+    if len(phone_nums) == 0:
+        print("No number was found")
+    else:
+    # TODO Else, break number up into area code, prefix, and suffic
+        for i in range(len(phone_nums)):
+            matches = re.search(r'(\d{3})[\s-]*(\d{3})[\s-]*(\d{4})', phone_nums[i])
+            print(f"Area Code: {matches.group(1)}")
+            print(f"Prefix: {matches.group(2)}")
+            print(f"Suffix: {matches.group(3)}")
+
    
     
-    # TODO Else, break number up into area code, prefix, and suffic
     
     
     # As a stretch goal, you can modify your regex to search for country codes
