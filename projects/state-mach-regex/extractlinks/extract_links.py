6b96ac85668a80a7a34e74a2f56739fa70c45abb
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-10-09 14:13:00 -0500

    Update projects/state-mach-regex/extractlinks/extract_links.py
    HTMLParser

--- projects/state-mach-regex/extractlinks/extract_links.py
+++ projects/state-mach-regex/extractlinks/extract_links.py
@@ -1,17 +1,34 @@
-import re # module for processing regular expressions https://docs.python.org/3/library/re.html
+import re  # module for processing regular expressions https://docs.python.org/3/library/re.html
 import sys
 import csv
+from html.parser import HTMLParser
+
 if __name__ == '__main__':
-  # Exit if command line args entered incorrectly
-  if len(sys.argv) != 2:
-    print("usage: extract_links.py [input_file]")
-    sys.exit(0)
+    # Exit if command line args entered incorrectly
+    if len(sys.argv) != 2:
+        print("usage: extract_links.py [input_file]")
+        sys.exit(0)
 
 # Filename is 2nd command line arg
 filename = sys.argv[1]
 
-# TODO Read HTML file
+# Read HTML file
 
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+
+parser = MyHTMLParser()
+parser.feed('<html><head><title>Test</title></head>'
+            '<body><h1>Parse me!</h1></body></html>')
 
 # TODO Set up regex
 
@@ -20,7 +37,7 @@ filename = sys.argv[1]
 
 
 # Check matches, print results
-# TODO Read in links from answers.txt (hint...this is a CSV file), 
+# TODO Read in links from answers.txt (hint...this is a CSV file),
 # save in list called 'answer_data'
 
 
