3a142d0d6e2ebd5ac05dfaa1c6e1ba3121ee8409
TangledTessellations <mcevoya@oregonstate.edu>
2018-10-09 15:40:23 -0700

    Close enough on extract_links.py

--- projects/state-mach-regex/extractlinks/extract_links.py
+++ projects/state-mach-regex/extractlinks/extract_links.py
@@ -1,6 +1,8 @@
 import re # module for processing regular expressions https://docs.python.org/3/library/re.html
 import sys
 import csv
+
+
 if __name__ == '__main__':
   # Exit if command line args entered incorrectly
   if len(sys.argv) != 2:
@@ -11,19 +13,62 @@ if __name__ == '__main__':
 filename = sys.argv[1]
 
 # TODO Read HTML file
+f = open(filename, 'r')
 
+contents = f.readlines()
 
 # TODO Set up regex
+regex = re.compile(r'http[s]?:\/\/.+?[\'"]')
 
+unformatted_matches = []
 
-# TODO Find links using regex, save in list called 'matches'
+for line in contents:
+  line_matches = re.findall(regex, line)
+  if(line_matches):
+    for link in line_matches:
+      unformatted_matches.append(line_matches)
 
+matches = []
+
+for item in unformatted_matches:
+  item = item[0][0:-1]
+  matches.append(item)
 
 # Check matches, print results
 # TODO Read in links from answers.txt (hint...this is a CSV file), 
 # save in list called 'answer_data'
+all_links = ''
+unformatted_answer_data = []
+
+with open('answers.txt', 'r') as csvfile:
+  spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+  for row in spamreader:
+    all_links = ', '.join(row)
+    unformatted_answer_data = all_links.split(',')
+
+answer_data = []
+
+for link in unformatted_answer_data:
+  link = link[1:-1]
+  answer_data.append(link)
+
+print("In answers and not in matches")
+for link in answer_data:
+  if link not in matches:
+    print(link)
+    print('\n\n')
+    
+print("In matches and not in answers")
+for link in matches:
+  if link not in answer_data:
+    print(link)
+    print('\n\n')
 
 
+print("Matches Length: ", len(matches))
+print("Answers Length: ", len(answer_data))
+
+  
 # Compare answers with matches found using regex, print out any mismatches
 # UNCOMMENT BELOW WHEN READY TO CHECK IF YOUR REGEX IS FINDING ALL THE LINKS
 # result = "All links matched!"
