e0257701339fc1a27d695bd4d3d9d220381e399d
Austin Merando <ajmerando1@yahoo.com>
2018-10-09 18:42:31 -0700

    fixed bug with displaying phone number breakdown

--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -8,7 +8,6 @@ reg = '^[0-9]{3}[-.)( ]*[0-9]{3}[-.)( ]*[0-9]{4}$'
 
 while line != "exit":
     # TODO Find matches
-    
     match = re.search(reg, line)
 
     # TODO If no match found, print that no number was found
@@ -19,9 +18,12 @@ while line != "exit":
     # TODO Else, break number up into area code, prefix, and suffic
     else:
         l = list(match.string)
+        for i in l:
+            if i is '-':
+                l.remove(i)
         print("Area code is {}{}{}".format(l[0],l[1],l[2]))
-        print("Prefix is {}{}{}".format(l[4],l[5],l[6]))
-        print("Suffix is {}{}{}{}".format(l[8],l[9],l[10],l[11]))
+        print("Prefix is {}{}{}".format(l[3],l[4],l[5]))
+        print("Suffix is {}{}{}{}".format(l[6],l[7],l[8],l[9]))
     
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
