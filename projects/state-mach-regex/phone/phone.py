a081c6e6b23e57a93df4bde952a8dd7c7ec8ec2e
emaquino44 <prexy44@yahoo.com>
2018-10-10 10:49:45 -0700

    Finished Extact_links - added solution

--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -9,17 +9,17 @@ line = input("Enter a phone number to validate or 'exit' when done. ")
 while line != "exit":
     # TODO Find matches
 
-    
+
     # TODO If no match found, print that no number was found
-   
-   
-    
+
+
+
     # TODO Else, break number up into area code, prefix, and suffic
-    
-    
+
+
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
-    
-    
+
+
     # Done validating, read in a new line
     line = input("Enter a phone number to validate or 'exit' when done. ")
