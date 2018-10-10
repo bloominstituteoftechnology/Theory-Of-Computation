9afa7593ed0bd06811b37586ad649424fe33209b
sibhat <sibhat.temelso@gmail.com>
2018-10-10 13:43:31 -0700

    mvp done

--- projects/state-mach-regex/extractlinks/extract_links.py
+++ projects/state-mach-regex/extractlinks/extract_links.py
@@ -1,37 +1,38 @@
 import re # module for processing regular expressions https://docs.python.org/3/library/re.html
 import sys
 import csv
+
+
 if __name__ == '__main__':
-  # Exit if command line args entered incorrectly
-  if len(sys.argv) != 2:
-    print("usage: extract_links.py [input_file]")
-    sys.exit(0)
+    # Exit if command line args entered incorrectly
+    if len(sys.argv) != 2:
+        print("usage: extract_links.py [input_file]")
+        sys.exit(0)
+    # Filename is 2nd command line arg
+    filename = sys.argv[1]
 
-# Filename is 2nd command line arg
-filename = sys.argv[1]
+    file = open(filename, 'r')
+    p = re.compile("https?://[a-z0-9-]+\..+?(?=\"|\')")
 
-# TODO Read HTML file
+    matches = []
+    for line in file:
+        matches.extend(p.findall(line))
 
+    file.close()
 
-# TODO Set up regex
+    answer_file = open("answers.txt", 'r')
+    answer_datas = csv.reader(answer_file, delimiter=",")
+    answer_data = []
+    for line in answer_datas:
+        answer_data.extend(line)
 
+    result = "All links matched!"
+    # answer_file.close()
+    if len(matches) != len(answer_data):
+        result = "Your regex found %i matches. There should be %i matches" % (len(matches), len(answer_data))
+    else:
+        for i in range(len(answer_data)):
+            if(matches[i] != answer_data[i]):
+                result = "Mismatched link. Got %s but expected %s" % (matches[i], answer_data[i])
+    print(result)
 
-# TODO Find links using regex, save in list called 'matches'
-
-
-# Check matches, print results
-# TODO Read in links from answers.txt (hint...this is a CSV file), 
-# save in list called 'answer_data'
-
-
-# Compare answers with matches found using regex, print out any mismatches
-# UNCOMMENT BELOW WHEN READY TO CHECK IF YOUR REGEX IS FINDING ALL THE LINKS
-# result = "All links matched!"
-# if len( matches ) != len( answer_data ):
-#   result = "Your regex found %i matches. There should be %i matches" %(len( matches ), len( answer_data ) )
-# else:
-#   for i in range( len(answer_data) ):
-#     if( matches[i] != answer_data[i] ):
-#       result = "Mismatched link. Got %s but expected %s" % ( matches[i], answer_data[i] )
-#       break
-# print( result )
--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -7,19 +7,22 @@ line = input("Enter a phone number to validate or 'exit' when done. ")
 
 
 while line != "exit":
-    # TODO Find matches
 
-    
-    # TODO If no match found, print that no number was found
-   
-   
-    
-    # TODO Else, break number up into area code, prefix, and suffic
-    
-    
-    # As a stretch goal, you can modify your regex to search for country codes
-    # too and print that out as well!
-    
-    
-    # Done validating, read in a new line
+    x = r"^[\(]*\d{3}[\) -]*\d{3}[- ]*\d{4}$"
+    matchs = re.match(x, line)
+    if matchs is None:
+        print("no match found")
+    else:
+        # TODO Else, break number up into area code, prefix, and suffic
+        numbers = [n for n in matchs.group() if n.isnumeric()]
+        area_code = numbers[:3]
+        prefix = numbers[3:6]
+        suffix = numbers[6:10]
+        print(f" Area code: {''.join(area_code)}\n Prefix: {''.join(prefix)}\n Suffix: {''.join((suffix))}\n")
+
+
+        # As a stretch goal, you can modify your regex to search for country codes
+        # too and print that out as well!
+
+        # Done validating, read in a new line
     line = input("Enter a phone number to validate or 'exit' when done. ")
