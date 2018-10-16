cc9f057797651bd2618a1928d574e864a423205a
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-16 10:35:20 -0700

    phone.py

--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -4,11 +4,12 @@ import re # module for processing regular expressions https://docs.python.org/3/
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # TODO Define your regex
-
+phoneRegex = '\+?([\d]{0,3})[-\s\(]?(\d{3})[\)\s\-]?\(?(\d{3})\s?\-?\)?\(?(\d{4})\)?'   # including parentheses
 
 while line != "exit":
     # TODO Find matches
-    match = re.match(r"", line)
+    match = re.search(phoneRegex, line)
+    
     
     # TODO If no match found, print that no number was found
    
@@ -17,6 +18,7 @@ while line != "exit":
     
     # TODO Else, break number up into area code, prefix, and suffic
     else:
+        print('Country code: %s Area code: %s  Prefix: %s  Suffix: %s ' % (match[1], match[2], match[3], match[4]))
     
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
