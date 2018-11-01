8288985ead93b06b627c4edbf113a8468aede773
FireInjun <mk@FireInjun.com>
2018-11-01 18:48:03 -0400

    11/1/2018 6:48 PM

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,4 +1,4 @@
-var readline = require('readline');
+var readline = require("readline");
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -6,18 +6,12 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-
 // This code reads a line at a time from stdin
 
-rl.on('line', function (line) {
-
-    // !!!! IMPLEMENT ME
-
-    // Come up with the phone regex
-
-    // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+rl.on("line", function(line) {
+  // !!!! IMPLEMENT ME
+  // Come up with the phone regex
+  // Find matches
+  // If match found, print number with no spaces, parens, or dashes
+  // Else print that no number was found
 });
