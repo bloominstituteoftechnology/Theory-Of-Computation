1cb0b904c74cfcdf7e2dc5e8d52654521c92e66f
mkerbleski <mkerbles@gmail.com>
2018-11-13 14:51:05 -0700

    complete first 2

--- project/state_machines/state_machines.c
+++ project/state_machines/state_machines.c
@@ -35,20 +35,28 @@ typedef struct StateMachine {
  ************************************/
 
 /*****
- * Allocate memory for a new  state machine
+ * Allocate memory for a new state machine
  *
  * TODO: FILL THIS IN
  *****/
 StateMachine *create_state_machine (int state_capacity, int transition_capacity) {
   // Allocate memory for state machine struct
-
+  StateMachine *sm = malloc(sizeof(StateMachine));
   // Current state should default to NULL
-
+  sm->current_state = NULL;
   // num_states and num_transitions should default to 0
+  sm->num_states = 0;
+  sm->num_transitions = 0;
 
   // Allocate memory for states
+  sm->state_capacity = state_capacity;
+  sm->states = calloc(state_capacity, sizeof(State *));
 
   // Allocate memory for transitions
+  sm->transition_capacity = transition_capacity;
+  sm->transitions = calloc(transition_capacity, sizeof(Transition *));
+
+  return sm;
 
 }
 
@@ -59,10 +67,13 @@ StateMachine *create_state_machine (int state_capacity, int transition_capacity)
  *****/
 State *create_state(char *name) {
   // Allocate memory for state struct
-
+  State * state = malloc(sizeof(State));
   // Allocate memory and copy state name (hint: use strdup)
-
+  state->name = strdup(name);
   // Set is_terminal to default of 0
+  state->is_terminal = 0;
+
+  return state;
 
 }
 
@@ -73,11 +84,14 @@ State *create_state(char *name) {
  *****/
 Transition *create_transition(char *name, State *origin, State *destination) {
   // Allocate memory for transition struct
-
+  Transition *transition = malloc(sizeof(Transition));
   // Allocate memory and copy transition name (hint: use strdup)
-
+  transition->name = strdup(name);
   // Set origin and destination states
+  transition->origin = origin;
+  transition->destination = destination;
 
+  return transition;
 }
 
 /*****
@@ -86,7 +100,10 @@ Transition *create_transition(char *name, State *origin, State *destination) {
  * TODO: FILL THIS IN
  *****/
 void destroy_state(State *state) {
-
+  if(state != NULL){
+    free(state->name);
+    free(state);
+  }
 }
 
 /*****
@@ -95,7 +112,10 @@ void destroy_state(State *state) {
  * TODO: FILL THIS IN
  *****/
 void destroy_transition(Transition *transition) {
-
+  if(transition != NULL){
+    free(transition->name);
+    free(transition);
+  }
 }
 
 /*****
@@ -106,10 +126,17 @@ void destroy_transition(Transition *transition) {
 void destroy_state_machine(StateMachine *sm) {
 
   // Free all transitions
-
+  for (int i = 0; i<sm->transition_capacity; i++){
+    destroy_transition(sm->transitions[i]);
+  }
   // Free all states
-
+  for (int j = 0; j<sm->state_capacity; j++){
+    destroy_state(sm->states[j]);
+  }
   // Free state machine
+  free(sm->transitions);
+  free(sm->states);
+  free(sm);
 }
 
 
@@ -128,15 +155,23 @@ void destroy_state_machine(StateMachine *sm) {
  *****/
 State *sm_add_state(StateMachine *sm, char *state_name) {
   // Return NULL and print an error if number of states is over capacity
-
+  if(sm->num_states > sm->state_capacity){
+    printf("error");
+    return NULL;
+  }
   // Return NULL and print an error if state name is not unique
-
+  for(int i=0; i < sm->state_capacity; i++){
+      if(state_name == sm->states->name){
+        printf("error");
+        return NULL;
+      }
+  }
   // Create a new state and add it to the state machine
-
+  sm->states[sm->num_states] = create_state(state_name);
   // Initialize the state machine's current state if it hasn't been set yet
-
+  sm->current_state = sm->states[sm->num_states];
   // Return the state
-  return state;
+  return sm->current_state;
 }
 
 /*****
@@ -148,10 +183,12 @@ State *sm_add_state(StateMachine *sm, char *state_name) {
 State *sm_add_terminal_state(StateMachine *sm, char *state_name) {
   // Add a state to the state machine
   // HINT: you can do this via the sm_add_state() function
-
+  sm->states[sm->num_states] = create_state(state_name);
   // If the new state is valid, set is_terminal to 1
-
-  return state;
+  if(sm->states[sm->num_states] == 30){
+    sm->current_state->is_terminal = 1;
+  }
+  return sm->states[1];
 }
 
 
@@ -288,7 +325,6 @@ int main(void)
   process_input(sm);
 
   destroy_state_machine(sm);
-
 
   return 0;
 }
