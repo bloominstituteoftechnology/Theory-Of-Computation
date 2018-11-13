518c7dab4434e14a64704bd452d9f766208c1a0a
sca4cs <shannon.atkinson.ca@gmail.com>
2018-11-13 15:01:45 -0500

    initial commit; lecture solution for day 1

--- project/state_machines/state_machines.c
+++ project/state_machines/state_machines.c
@@ -6,7 +6,7 @@
 typedef struct State {
   char *name;
   int is_terminal;  // 1 if this is a terminating state; 0 otherwise
-} State;
+} State; // alias for struct State
 
 typedef struct Transition {
   char *name;
@@ -19,7 +19,7 @@ typedef struct StateMachine {
 
   int state_capacity;  // How many states can this state machine hold?
   int num_states;  // How many states does the state machine currently hold
-  struct State **states;
+  struct State **states; // list of pointers
 
   int transition_capacity;  // How many transitions can this state machine hold?
   int num_transitions;  // How many transitions does the state machine currently hold
@@ -41,15 +41,20 @@ typedef struct StateMachine {
  *****/
 StateMachine *create_state_machine (int state_capacity, int transition_capacity) {
   // Allocate memory for state machine struct
-
+  StateMachine *sm = malloc(sizeof(StateMachine)); // sm is a pointer to the state machine
   // Current state should default to NULL
-
+  sm->current_state = NULL; // NULL gives you zeros
   // num_states and num_transitions should default to 0
-
+  sm->num_states = 0;
+  sm->num_transitions = 0;
   // Allocate memory for states
-
+  sm->state_capacity = state_capacity;
+  sm->states = calloc(state_capacity, sizeof(State *));
   // Allocate memory for transitions
+  sm->transition_capacity = transition_capacity;
+  sm->transitions = calloc(transition_capacity, sizeof(Transition *));
 
+  return sm;
 }
 
 /*****
@@ -59,11 +64,13 @@ StateMachine *create_state_machine (int state_capacity, int transition_capacity)
  *****/
 State *create_state(char *name) {
   // Allocate memory for state struct
-
+  State *state = malloc(sizeof(State));
   // Allocate memory and copy state name (hint: use strdup)
-
+  state->name = strdup(name);
   // Set is_terminal to default of 0
+  state->is_terminal = 0;
 
+  return state;
 }
 
 /*****
@@ -73,11 +80,14 @@ State *create_state(char *name) {
  *****/
 Transition *create_transition(char *name, State *origin, State *destination) {
   // Allocate memory for transition struct
-
+  Transition *transition = malloc(sizeof(Transition));
   // Allocate memory and copy transition name (hint: use strdup)
-
+  transition->name = strdup(name);
   // Set origin and destination states
+  transition->origin = origin;
+  transition->destination = destination;
 
+  return transition;
 }
 
 /*****
@@ -86,7 +96,10 @@ Transition *create_transition(char *name, State *origin, State *destination) {
  * TODO: FILL THIS IN
  *****/
 void destroy_state(State *state) {
-
+  if (state != NULL) {
+    free(state->name);
+    free(state);
+  }
 }
 
 /*****
@@ -95,7 +108,10 @@ void destroy_state(State *state) {
  * TODO: FILL THIS IN
  *****/
 void destroy_transition(Transition *transition) {
-
+  if (transition != NULL) {
+      free(transition->name);
+      free(transition);
+    }
 }
 
 /*****
@@ -106,10 +122,17 @@ void destroy_transition(Transition *transition) {
 void destroy_state_machine(StateMachine *sm) {
 
   // Free all transitions
-
+  for (int i = 0; i < sm->transition_capacity; i++) {
+    destroy_transition(sm->transitions[i]);
+  }
   // Free all states
-
+  for (int j = 0; j < sm->state_capacity; j++) {
+    destroy_state(sm->states[j]);
+  }
   // Free state machine
+  free(sm->transitions);
+  free(sm->states);
+  free(sm);
 }
 
 
@@ -136,7 +159,7 @@ State *sm_add_state(StateMachine *sm, char *state_name) {
   // Initialize the state machine's current state if it hasn't been set yet
 
   // Return the state
-  return state;
+  //  return state;
 }
 
 /*****
@@ -151,7 +174,7 @@ State *sm_add_terminal_state(StateMachine *sm, char *state_name) {
 
   // If the new state is valid, set is_terminal to 1
 
-  return state;
+  //  return state;
 }
 
 
