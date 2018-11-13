042e09ec62ce973864f17868053c3e43490377ab
vanceleon <vanceleon44@gmail.com>
2018-11-13 16:24:11 -0500

    setting up pull day 1 code inputed and studied

--- project/state_machines/state_machines.c
+++ project/state_machines/state_machines.c
@@ -41,15 +41,22 @@ typedef struct StateMachine {
  *****/
 StateMachine *create_state_machine (int state_capacity, int transition_capacity) {
   // Allocate memory for state machine struct
-
+  StateMachine *sm = malloc(sizeof(StateMachine));
   // Current state should default to NULL
+  sm->current_state = NULL;
 
   // num_states and num_transitions should default to 0
+  sm->num_states = 0;
+  sm->num_transitions = 0;
 
   // Allocate memory for states
-
+  sm->state_capacity = transition_capacity;
+  sm->states = calloc(state_capacity, sizeof(State *));
   // Allocate memory for transitions
+  sm->transition_capacity = transition_capacity;
+  sm->transitions = calloc(transition_capacity, sizeof(Transition *));
 
+  return sm;
 }
 
 /*****
@@ -59,11 +66,11 @@ StateMachine *create_state_machine (int state_capacity, int transition_capacity)
  *****/
 State *create_state(char *name) {
   // Allocate memory for state struct
-
+  State *state = malloc(sizeof(State));
   // Allocate memory and copy state name (hint: use strdup)
-
+  state->name = strdup(name);
   // Set is_terminal to default of 0
-
+  state->is_terminal = 0;
 }
 
 /*****
@@ -73,11 +80,14 @@ State *create_state(char *name) {
  *****/
 Transition *create_transition(char *name, State *origin, State *destination) {
   // Allocate memory for transition struct
-
+  Transition *transition = malloc(sizeof(Transition));
   // Allocate memory and copy transition name (hint: use strdup)
-
+  transition->name = strdup(name);
   // Set origin and destination states
+  transition->origin = origin;
+  transition->destination = destination;
 
+  return transition;
 }
 
 /*****
@@ -86,7 +96,10 @@ Transition *create_transition(char *name, State *origin, State *destination) {
  * TODO: FILL THIS IN
  *****/
 void destroy_state(State *state) {
-
+  if(state != NULL) {
+    free(state->name);
+    free(state);
+  }
 }
 
 /*****
@@ -95,7 +108,10 @@ void destroy_state(State *state) {
  * TODO: FILL THIS IN
  *****/
 void destroy_transition(Transition *transition) {
-
+  if(transition != NULL) {
+    free(transition->name);
+    free(transition);
+  }
 }
 
 /*****
@@ -106,10 +122,18 @@ void destroy_transition(Transition *transition) {
 void destroy_state_machine(StateMachine *sm) {
 
   // Free all transitions
-
+  for(int i = 0; i < sm->transition_capacity; i++) {
+    destroy_transition(sm->transitions[i]);
+  }
   // Free all states
-
+  for(int j = 0; j < sm->state_capacity; j++){
+    destroy_state(sm->status[j]);
+  }
   // Free state machine
+
+  free(sm->transitions);
+  free(sm->states);
+  free(sm);
 }
 
 
