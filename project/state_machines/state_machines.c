271a6edd4a306b7d71ce49d864987156f025a6b1
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-13 14:45:24 -0800

    completed sm_add_state method

--- project/state_machines/state_machines.c
+++ project/state_machines/state_machines.c
@@ -152,13 +152,25 @@ void destroy_state_machine(StateMachine *sm) {
  *****/
 State *sm_add_state(StateMachine *sm, char *state_name) {
   // Return NULL and print an error if number of states is over capacity
-
+	if(sm->num_states == sm->state_capacity) {
+		printf("Number of states is over capacity");	
+		return NULL;
+	}		
   // Return NULL and print an error if state name is not unique
-
+	for(int i=0; i < sm->num_states; i++){
+		if(sm->states[i]->name==state_name) {
+			printf("State name is not unique");
+                	return NULL;
+		}
+	}
   // Create a new state and add it to the state machine
-
+	State *state = create_state(char state_name);
+	sm->states[sm->num_states] = state;
+	sm->num_states+=1;
   // Initialize the state machine's current state if it hasn't been set yet
-
+	if(sm->current_state == NULL){
+		sm->current_state = state;
+	}
   // Return the state
   return state;
 }
