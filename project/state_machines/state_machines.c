97e7c9a8d0e05881613c7ebb7b3d63a10339128d
DasGMA <dasgrigoma@gmail.com>
2018-11-13 16:29:57 -0800

    Update.

--- project/state_machines/state_machines.c
+++ project/state_machines/state_machines.c
@@ -151,7 +151,7 @@ void destroy_state_machine(StateMachine *sm) {
  *****/
 State *sm_add_state(StateMachine *sm, char *state_name) {
   // Return NULL and print an error if number of states is over capacity
-  if (sm -> num_states == sm -> state_capacity) {
+  if (sm -> num_states >= sm -> state_capacity) {
     perror("Full capacity reached.\n");
     return NULL;
   }
@@ -201,7 +201,7 @@ Transition *sm_add_transition(StateMachine *sm, char *transition_name,
                               char *origin_state_name, char *destination_state_name) {
 
   // Return NULL and print an error if number of transitions is over capacity
-  if (sm -> num_transitions == sm -> transition_capacity) {
+  if (sm -> num_transitions >= sm -> transition_capacity) {
     perror("Full capacity reached.\n");
     return NULL;
   }
