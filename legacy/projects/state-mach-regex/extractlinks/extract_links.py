e0f2ff1b3fff5ae164334ccd88da2fea2208d901
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-17 16:02:29 +0200

    match links

--- legacy/projects/state-mach-regex/extractlinks/extract_links.py
+++ legacy/projects/state-mach-regex/extractlinks/extract_links.py
@@ -1,37 +1,45 @@
-import re # module for processing regular expressions https://docs.python.org/3/library/re.html
+import re  # module for processing regular expressions https://docs.python.org/3/library/re.html
 import sys
 import csv
-if __name__ == '__main__':
-  # Exit if command line args entered incorrectly
-  if len(sys.argv) != 2:
-    print("usage: extract_links.py [input_file]")
-    sys.exit(0)
+# if __name__ == '__main__':
+#     # Exit if command line args entered incorrectly
+#     if len(sys.argv) != 2:
+#         print("usage: extract_links.py [input_file]")
+#         sys.exit(0)
 
-# Filename is 2nd command line arg
-filename = sys.argv[1]
+# # Filename is 2nd command line arg
+# filename = sys.argv[1]
 
-# TODO Read HTML file
+# Read HTML file
+with open("stackoverflow.html", encoding="utf8") as stack_overflow_links:
+    links = stack_overflow_links.read()
 
 
-# TODO Set up regex
+# Set up regex
+regex_links = r"\b(https?:\/\/[A-z0-9.-]+[.][#@&%sA;-z0-9./?=~-]+)"
 
-
-# TODO Find links using regex, save in list called 'matches'
-
+# Find links using regex, save in list called 'matches'
+matches = re.findall(regex_links, links)
+# print(matches)
 
 # Check matches, print results
-# TODO Read in links from answers.txt (hint...this is a CSV file), 
+# Read in links from answers.txt (hint...this is a CSV file),
 # save in list called 'answer_data'
+with open('answers.txt', encoding="utf8") as so_answers:
+    answers = so_answers.read().split(',')
+    answer_data = [answer.replace('"', '') for answer in answers]
 
 
 # Compare answers with matches found using regex, print out any mismatches
 # UNCOMMENT BELOW WHEN READY TO CHECK IF YOUR REGEX IS FINDING ALL THE LINKS
-# result = "All links matched!"
-# if len( matches ) != len( answer_data ):
-#   result = "Your regex found %i matches. There should be %i matches" %(len( matches ), len( answer_data ) )
-# else:
-#   for i in range( len(answer_data) ):
-#     if( matches[i] != answer_data[i] ):
-#       result = "Mismatched link. Got %s but expected %s" % ( matches[i], answer_data[i] )
-#       break
-# print( result )
+result = "All links matched!"
+if len(matches) != len(answer_data):
+    result = "Your regex found %i matches. There should be %i matches" % (
+        len(matches), len(answer_data))
+else:
+    for i in range(len(answer_data)):
+        if(matches[i] != answer_data[i]):
+            result = "Mismatched link. Got %s but expected %s" % (
+                matches[i], answer_data[i])
+            break
+print(result)
