47c6730faec39b7c8e8e58074ea6ad623d985629
cocoitali <cmburatto@gmail.com>
2019-07-31 22:54:29 -0600

    phone.py : playing around with different logic

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -4,23 +4,32 @@ import re # module for processing regular expressions https://docs.python.org/3/
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # TODO Define your regex
-const phoneRegex = ^\+?\d{0,3}\s?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}?
+# const phoneRegex = ^\+?\d{0,3}\s?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}?
 
 
 while line != "exit":
     # TODO Find matches
-    for line in lines:
-        line = str(line)
-        if phoneRegex.match(line)
-        print(line)
+    # for line in lines:
+    #     line = str(line)
+
+    #     if phoneRegex.match(line)
+    #     print(line)
+    # for _ in range(int(input())):
+    for l in line:
+        # line = input()
+        if re.match(r"^\+?\d{0,3}\s?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}?", line):
+            print("YES")
+        else:
+            print("No number was found")
+
     
     # TODO If no match found, print that no number was found
-        if not match:
-            print("No number was found")
+        # if not match:
+        #     print("No number was found")
    
     
     # TODO Else, break number up into area code, prefix, and suffic
-        else:
+        
 
     
     # As a stretch goal, you can modify your regex to search for country codes
