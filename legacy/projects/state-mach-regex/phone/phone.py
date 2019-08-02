d520a8ae79ae8a787f9eb64fb53ca7a7c7a70498
Alex Martin <atonymartin20@yahoo.com>
2019-08-01 21:52:40 -0500

    Revert "Revert "completed MVP on phone.py""
    This reverts commit 31b0171e0d142b2bfc6c8d4142bce9004d5f1aad.

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -4,18 +4,29 @@ import re # module for processing regular expressions https://docs.python.org/3/
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # TODO Define your regex
-
+phone_ex = r"[0-9]{3}[-]*[)]*[ ]*[0-9]{3}[-]*[ ]*[0-9]{4}"
+# This should work for any phone number formatted like these -> 555-123-4567, 5551234567, (555) 123-4567, 555 123 4567
 
 while line != "exit":
     # TODO Find matches
-
+    if re.findall(phone_ex, line):
+        phone_num = re.findall(phone_ex, line)
+        print('\nline = ', line)
+        print(phone_num)
+    # TODO Else, break number up into area code, prefix, and suffix
+        for phoneNum in phone_num:
+            num2 =''.join(i for i in phone_num if i.isdigit())
+            print(num2)
+            areaCode = num2[0:3]
+            prefix = num2[3:6]
+            suffix = num2[6:]
+            print('Area code:', areaCode, '\nPrefix:', prefix, '\nSuffix:', suffix, '\n')
+    
     
     # TODO If no match found, print that no number was found
-   
-   
-    
-    # TODO Else, break number up into area code, prefix, and suffic
-    
+
+    else:
+        print('No number was found.')
     
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
