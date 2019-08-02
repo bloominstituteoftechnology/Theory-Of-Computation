e37b0ad2e37f801b11f9e8e319925c3fee07858c
Michael Trew <mtrew2015@gmail.com>
2019-08-01 22:16:41 -0400

    completed

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -6,22 +6,34 @@ phone = input("Enter a phone number to validate or 'exit' when done. ")
 
 dashed_regex = r"[0-9]{3}[-]{1}[0-9]{3}[-]{1}[0-9]{4}"
 
-matches = re.finditer(dashed_regex, phone, flags=0)
+numbers_only = r"[0-9]{10}"
+
+numbers_only_matches = re.findall(numbers_only, phone, flags=0)
+
+matches = re.findall(dashed_regex, phone, flags=0)
 
 while phone != "exit":
     # TODO Find matches
 
     if matches:
         print('valid phone number')
+        break
    
 # TODO If no match found, print that no number was found
 
-    else:
-        print('invaid phone number')
+    elif numbers_only_matches:
+        print(re.sub(r'(\d{3})(\d{3})(\d{4})', r'(\1)\2-\3', phone))
+        break
+        
 
  # TODO Else, break number up into area code, prefix, and suffic
-    
-    
+
+    else:
+        print('invaid phone number')
+        break
+        
+
+
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
     
