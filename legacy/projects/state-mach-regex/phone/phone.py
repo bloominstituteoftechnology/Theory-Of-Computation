b2a94eed1051b1743cfd857105cbe754af204502
unknown <rlmclaughlin@yahoo.com>
2019-08-01 21:58:28 -0500

    finished phone.py

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -8,19 +8,20 @@ phone_ex = r"([0-9]{3}[-][0-9]{3}[-][0-9]{4}|[0-9]{10}|[(][0-9]{3}[)]\s[0-9]{3}[
 
 
 while line != "exit":
-    # TODO Find matches
 
+    if re.findall(phone_ex, line):
+        num = ''.join(e for e in line if e.isalnum())
+        area = num[:3]
+        prefix = num[3:6]
+        suffix = num[6:]
+        print('Area Code:', area, '\nPrefix:', prefix, '\nSuffix:', suffix)
+    else:
+        print('Unable to find the number')
     
-    # TODO If no match found, print that no number was found
+        line = input("Enter a phone number to validate or 'exit' when done. ")
    
    
-    
-    # TODO Else, break number up into area code, prefix, and suffic
-    
-    
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
     
-    
     # Done validating, read in a new line
-    line = input("Enter a phone number to validate or 'exit' when done. ")
